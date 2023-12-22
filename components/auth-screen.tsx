2f8fc9234006c934d503e3859530e57b6f2325bb
kdu2030 <kdu2030@gmail.com>
2023-12-21 22:17:39 -0500

    Add Auth Screen Styles

--- components/auth-screen.tsx
+++ components/auth-screen.tsx
@@ -1,17 +1,29 @@
 import * as React from "react";
 import { Text, Button } from "react-native-paper"
 import { View } from "react-native";
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 
 
 export function AuthScreen() {
+    const backgroundImageSrc = {
+        uri: "https://cdn.pixabay.com/photo/2020/03/03/20/31/boat-4899802_1280.jpg"
+    }
+
+
     return (
-        <View>
-            <Text variant="headlineMedium">Prayer Requests App</Text>
-            <View className="w-9/12 mt-3">
-                <Button mode="contained">Sign up</Button>
+        <ImageBackground source={backgroundImageSrc} className="flex flex-1">
+            <View className="flex bg-white mt-auto h-1/2 rounded-xl items-center">
+                <View className="h-full">
+                    <Text variant="displaySmall" className="font-bold mt-5">Welcome to</Text>
+                    <Text variant="displaySmall" className="font-bold">Prayer Requests App</Text>
+                    <Text variant="bodyLarge" className="text-gray-500 mt-3">The easier way to manage your prayer life</Text>
+                    <View className="flex flex-row w-full mt-auto mb-10">
+                        <Button mode="contained" className="w-2/5 mr-3">Sign up</Button>
+                        <Button mode="outlined" className="w-2/5">Sign in</Button>
+                    </View>
+                </View>
             </View>
-        </View>
+        </ImageBackground>
     )
 
 }

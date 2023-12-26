b4c40f0356e55546738685c683cbe59d801f57d8
kdu2030 <kdu2030@gmail.com>
2023-12-25 20:08:18 -0500

    Add App Container Tests

---
+++ tests/app-container.test.tsx
@@ -0,0 +1,76 @@
+jest.useFakeTimers();
+
+import * as React from "react";
+import { mountComponent } from "./utils/test-utils";
+import { RenderResult, waitFor } from "@testing-library/react-native";
+import AppContainer from "../app/index";
+import { SupportedLanguages } from "../types/languages";
+
+let component: RenderResult;
+const mockLoadLanguage = jest.fn();
+const mockRouterPush = jest.fn();
+
+jest.mock("expo-router/entry", () => ({
+  match: jest.fn(),
+}));
+
+jest.mock("expo", () => ({
+  registerRootComponent: jest.fn(),
+  match: jest.fn(),
+}));
+
+jest.mock("expo-router", () => ({
+  router: {
+    push: (path: string) => mockRouterPush(path),
+  },
+}));
+
+jest.mock("../hooks/use-i18n", () => ({
+  useI18N: () => ({
+    loadLanguage: () => mockLoadLanguage(),
+    translate: () => "",
+  }),
+}));
+
+describe("App Container Tests", () => {
+  afterEach(() => {
+    if (component) {
+      component.unmount();
+      mockLoadLanguage.mockReset();
+      mockRouterPush.mockReset();
+    }
+  });
+
+  test("Language unable to load", async () => {
+    mockLoadLanguage.mockReturnValue({ isError: true });
+    component = mountComponent(<AppContainer />);
+
+    await waitFor(() => {
+      expect(mockRouterPush).toHaveBeenCalledWith("/language/language-picker");
+    });
+  });
+
+  test("No language picked", async () => {
+    mockLoadLanguage.mockReturnValue({ isError: false });
+    component = mountComponent(<AppContainer />);
+
+    await waitFor(() => {
+      expect(mockRouterPush).toHaveBeenCalledWith("/language/language-picker");
+    });
+  });
+
+  test("Language picked but not signed in", async () => {
+    mockLoadLanguage.mockReturnValue({
+      isError: false,
+      value: SupportedLanguages.English,
+    });
+
+    component = mountComponent(<AppContainer />);
+
+    // Need update when user sign in check added
+
+    await waitFor(() => {
+      expect(mockRouterPush).toHaveBeenCalledWith("/auth/welcome");
+    });
+  });
+});

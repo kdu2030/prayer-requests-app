f8017b9109da61c33c0e20223c2439627585262b
kdu2030 <kdu2030@gmail.com>
2024-02-11 21:42:59 -0500

    Add Valid Form Submit Test

--- tests/sign-up.test.tsx
+++ tests/sign-up.test.tsx
@@ -8,15 +8,45 @@ import Signup from "../app/auth/sign-up";
 import { mountComponent } from "./utils/test-utils";
 import { SupportedLanguages } from "../types/languages";
 import { SignupTestIds } from "../constants/auth/auth-constants";
+import { SignupForm } from "../types/forms/signup-form";
+import { ApiSignupResponse } from "../api/post-signup";
+import { ApiResponse } from "../types/api-response-types";
+import { userTokenPair } from "./mock-data/mock-tokens";
+
+const mockPostSignup = jest.fn();
 
 jest.mock("@react-native-async-storage/async-storage", () => ({
-  default: {
-    setItem: jest.fn(),
-    getItem: jest.fn(),
-  },
+  ...jest.requireActual(
+    "@react-native-async-storage/async-storage/jest/async-storage-mock"
+  ),
+}));
+
+jest.mock("../api/post-signup", () => ({
+  postSignup: () => mockPostSignup(),
 }));
 
 let component: RenderResult;
+
+const addInputToSignupForm = ({
+  email,
+  password,
+  confirmPassword,
+  username,
+}: SignupForm) => {
+  const usernameInput = component.getByTestId(SignupTestIds.usernameInput);
+  fireEvent.changeText(usernameInput, username);
+
+  const emailInput = component.getByTestId(SignupTestIds.emailInput);
+  fireEvent.changeText(emailInput, email);
+
+  const passwordInput = component.getByTestId(SignupTestIds.passwordInput);
+  fireEvent.changeText(passwordInput, password);
+
+  const confirmPasswordInput = component.getByTestId(
+    SignupTestIds.confirmPasswordInput
+  );
+  fireEvent.changeText(confirmPasswordInput, confirmPassword);
+};
 
 describe("Signup Form Tests", () => {
   beforeEach(() => {
@@ -27,21 +57,44 @@ describe("Signup Form Tests", () => {
     if (component) {
       component.unmount();
     }
+    jest.resetAllMocks();
   });
 
   test("Component mounts", () => {
     expect(component).not.toBeNull();
   });
 
-  test("Invalid email prevents submit", async () => {
-    const invalidEmail = "gmail.com";
-    const emailInput = component.getByTestId(SignupTestIds.emailInput);
-    fireEvent.changeText(emailInput, invalidEmail);
+  test("Blank form prevents submit", async () => {
+    const submitButton = component.getByTestId(SignupTestIds.submitButton);
+    fireEvent.press(submitButton);
 
-    const submitButton = await component.findByTestId(
-      SignupTestIds.submitButton
-    );
+    await waitFor(() => {
+      expect(mockPostSignup).not.toHaveBeenCalled();
+    });
+  });
 
-    console.log(submitButton.props);
+  test("Valid Form is Submitted", async () => {
+    const validSignupForm: SignupForm = {
+      username: "testUsername",
+      email: "test@test.com",
+      password: "testPassword",
+      confirmPassword: "testPassword",
+    };
+
+    const mockResponse: ApiResponse<ApiSignupResponse> = {
+      isError: false,
+      value: userTokenPair,
+    };
+
+    mockPostSignup.mockReturnValueOnce(mockResponse);
+
+    addInputToSignupForm(validSignupForm);
+
+    const submitButton = component.getByTestId(SignupTestIds.submitButton);
+    fireEvent.press(submitButton);
+
+    await waitFor(() => {
+      expect(mockPostSignup).toHaveBeenCalled();
+    });
   });
 });

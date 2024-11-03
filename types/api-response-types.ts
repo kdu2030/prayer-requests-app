7e52fc9a8a89004e50414b5849979c74082896d4
kdu2030 <kdu2030@gmail.com>
2024-11-03 14:30:32 -0500

    Add New Error Response Types

--- types/api-response-types.ts
+++ types/api-response-types.ts
@@ -15,3 +15,16 @@ export type ApiResponse<ValueType> =
       isError: false;
       value: ValueType;
     };
+
+export enum ApiErrorCode {
+  GeneralError = "GEN_ERROR",
+  DataValidationError = "DATA_VALIDATION_ERROR",
+}
+
+export type ApiErrorResponse = {
+  errorCode: ApiErrorCode;
+  message?: string;
+  url?: string;
+  reqMethod?: string;
+  dataValidationErrors?: string[];
+};

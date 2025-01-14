e1c203c41274a73909e9287a41c387c6a2fd2c45
Kevin Du <kdu@costar.com>
2025-01-14 13:13:57 -0500

    Add Authorization Header Middleware Draft

--- hooks/use-api-data.tsx
+++ hooks/use-api-data.tsx
@@ -1,9 +1,12 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from "axios";
 import { router } from "expo-router";
 import * as React from "react";
 
+import { getUserTokenPair } from "../api/get-user-token-pair";
 import { REFRESH_TOKEN_STORAGE_KEY } from "../components/authentication/auth-constants";
 import { decodeJwtToken } from "../components/authentication/auth-helpers";
+import { BEARER_PREFIX } from "../constants/auth-constants";
 import {
   ApiDataContextType,
   UserData,
@@ -46,6 +49,7 @@ export const ApiDataContextProvider: React.FC<Props> = ({ children }) => {
 
 export const useApiDataContext = () => {
   const apiDataContext = React.useContext(ApiDataContext);
+  const fetchRef = React.useRef<AxiosInstance>(axios.create());
 
   const initializeContextFromStorage = async () => {
     const refreshToken = await AsyncStorage.getItem(REFRESH_TOKEN_STORAGE_KEY);
@@ -64,5 +68,60 @@ export const useApiDataContext = () => {
     }
   };
 
-  return { ...apiDataContext, initializeContextFromStorage };
+  const addAuthorizationHeader = async (config: InternalAxiosRequestConfig) => {
+    const { userTokens, baseUrl, setUserTokens } = apiDataContext;
+    const currentDate = new Date();
+    let currentUserTokens: UserTokenPair = { ...userTokens };
+
+    if (
+      !userTokens?.refreshToken ||
+      !userTokens.refreshTokenExpiryDate ||
+      userTokens.refreshTokenExpiryDate < currentDate
+    ) {
+      router.push("/auth/welcome");
+      throw new Error("Refresh token is expired.");
+    }
+
+    if (
+      !userTokens.accessToken ||
+      !userTokens.accessTokenExpiryDate ||
+      userTokens.accessTokenExpiryDate < currentDate
+    ) {
+      const response = await getUserTokenPair(baseUrl, userTokens.refreshToken);
+      if (response.isError) {
+        router.push("/auth/welcome");
+        throw new Error("Unable to fetch user tokens.");
+      }
+
+      const updatedAccessToken = decodeJwtToken(
+        response.value?.accessToken ?? ""
+      );
+      const updatedRefreshToken = decodeJwtToken(
+        response.value?.refreshToken ?? ""
+      );
+
+      currentUserTokens = {
+        accessToken: updatedAccessToken.token,
+        accessTokenExpiryDate: updatedAccessToken.tokenExpiryDate,
+        refreshToken: updatedRefreshToken.token,
+        refreshTokenExpiryDate: updatedRefreshToken.tokenExpiryDate,
+      };
+
+      setUserTokens(currentUserTokens);
+    }
+
+    config.headers.setAuthorization(
+      `${BEARER_PREFIX} ${currentUserTokens.accessToken}`
+    );
+
+    return config;
+  };
+
+  fetchRef.current.interceptors.request.use(addAuthorizationHeader);
+
+  return {
+    ...apiDataContext,
+    initializeContextFromStorage,
+    fetch: fetchRef.current,
+  };
 };

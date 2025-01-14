175820896c19329753e21ed633e60d27f46c1677
Kevin Du <kdu@costar.com>
2025-01-14 17:20:39 -0500

    Add Validate Refresh Token Helper

--- helpers/api-helpers.ts
+++ helpers/api-helpers.ts
@@ -1,5 +1,7 @@
 import { AxiosError } from "axios";
+import { router } from "expo-router";
 
+import { UserTokenPair } from "../types/context/api-data-context-type";
 import { ManagedErrorResponse } from "../types/error-handling";
 
 export const handleApiErrors = <TResponse>(
@@ -7,4 +9,17 @@ export const handleApiErrors = <TResponse>(
 ): ManagedErrorResponse<TResponse> => {
   const axiosError = error as AxiosError;
   return { isError: true, error: axiosError?.response?.data };
+};
+
+export const validateRefreshToken = (userTokens: UserTokenPair) => {
+  const currentDate = new Date();
+
+  if (
+    !userTokens?.refreshToken ||
+    !userTokens.refreshTokenExpiryDate ||
+    userTokens.refreshTokenExpiryDate < currentDate
+  ) {
+    router.push("/auth/welcome");
+    throw new Error("Refresh token is expired.");
+  }
 };

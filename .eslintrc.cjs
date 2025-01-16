97f2de5b54ba6a226e627e64b6ddf170a94dd1be
Kevin Du <kdu@costar.com>
2025-01-16 09:21:03 -0500

    Fix ESLint Rule for Unused Variables

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,5 +8,6 @@ module.exports = {
     "no-unused-vars": "warn",
     "@typescript-eslint/no-explicit-any": "off",
     "simple-import-sort/imports": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
   },
 };

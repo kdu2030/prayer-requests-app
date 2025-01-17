be7cf445c96b6ff1897ea2171a55c39b4c3999e0
Kevin Du <kdu@costar.com>
2025-01-17 09:10:13 -0500

    Add Custom Drawer Content

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,10 +1,18 @@
 import { Drawer } from "expo-router/drawer";
 import * as React from "react";
 
+import { DrawerContent } from "../../components/navigation/drawer-content";
 import { DrawerHeader } from "../../components/navigation/drawer-header";
 
 const DrawerLayout: React.FC = () => {
-  return <Drawer screenOptions={{ header: DrawerHeader }} />;
+  return (
+    <Drawer
+      screenOptions={{
+        header: DrawerHeader,
+      }}
+      drawerContent={DrawerContent}
+    />
+  );
 };
 
 export default DrawerLayout;
---
+++ components/navigation/drawer-content.tsx
@@ -0,0 +1,41 @@
+import {
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  useDrawerStatus,
+} from "@react-navigation/drawer";
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import { View } from "react-native";
+import { useTheme } from "react-native-paper";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+import { useApiDataContext } from "../../hooks/use-api-data";
+import { ProfilePicture } from "../layouts/profile-picture";
+
+export const DrawerContent: React.FC<DrawerContentComponentProps> = ({
+  ...props
+}) => {
+  const { userData } = useApiDataContext();
+  const theme = useTheme();
+  const insets = useSafeAreaInsets();
+  const drawerStatus = useDrawerStatus();
+
+  return (
+    <>
+      {drawerStatus === "open" && (
+        <StatusBar backgroundColor={theme.colors.primary} />
+      )}
+      <DrawerContentScrollView
+        {...props}
+        contentContainerStyle={{ paddingTop: insets.top }}
+      >
+        <View
+          className="flex flex-col items-center justify-center m-0"
+          style={{ backgroundColor: theme.colors.primary }}
+        >
+          <ProfilePicture url={userData?.image?.url} width={48} height={48} />
+        </View>
+      </DrawerContentScrollView>
+    </>
+  );
+};

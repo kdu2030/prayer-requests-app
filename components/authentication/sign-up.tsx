60b58f8ac2d015bd90e373ce1276c9ed5d1ca99b
kdu2030 <kdu2030@gmail.com>
2025-01-17 19:24:12 -0500

    Fix Signup Styling

--- components/authentication/sign-up.tsx
+++ components/authentication/sign-up.tsx
@@ -3,7 +3,7 @@ import { StatusBar } from "expo-status-bar";
 import { Formik, FormikProps, setNestedObjectValues } from "formik";
 import { isEmpty } from "lodash";
 import * as React from "react";
-import { ScrollView, View } from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
 import { Button, Snackbar, Text, useTheme } from "react-native-paper";
 
 import { postSignup } from "../../api/post-signup";
@@ -75,127 +75,130 @@ const Signup: React.FC = () => {
 
   return (
     <>
-      {/* <StatusBar backgroundColor={theme.colors.background} /> */}
-      <ScrollView
-        contentContainerStyle={{
-          display: "flex",
-          flexGrow: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: theme.colors.background,
-        }}
-      >
-        <View className="flex flex-col" style={{ minWidth: "80%" }}>
-          <Formik
-            initialValues={{}}
-            onSubmit={() => {}}
-            validationSchema={signupValidationSchema(translate)}
-            validateOnBlur
-            validateOnChange={false}
-          >
-            {(props) => (
-              <>
-                <Text
-                  variant="displaySmall"
-                  className="font-bold"
-                  style={{ color: theme.colors.primary }}
-                >
-                  {translate("common.appName")}
-                </Text>
-
-                <Text className="mt-5 mb-5 text-lg font-bold">
-                  {translate("signup.createAccount.label")}
-                </Text>
-
-                <TextInput
-                  name="username"
-                  label={translate("signup.username.label")}
-                  mode={"flat"}
-                  containerClassNames="mb-5"
-                  testID={SignupTestIds.usernameInput}
-                  required
-                />
-
-                <TextInput
-                  name="fullName"
-                  label={translate("signup.displayName.label")}
-                  mode={"flat"}
-                  containerClassNames="mb-5"
-                  testID={SignupTestIds.displayNameInput}
-                  required
-                />
-
-                <TextInput
-                  name="email"
-                  label={translate("signup.email.label")}
-                  containerClassNames="mb-5"
-                  mode={"flat"}
-                  testID={SignupTestIds.emailInput}
-                  required
-                />
-
-                <TextInput
-                  name="password"
-                  label={translate("signup.password.label")}
-                  containerClassNames="mb-5"
-                  mode={"flat"}
-                  secureTextEntry={true}
-                  testID={SignupTestIds.passwordInput}
-                  required
-                />
-
-                <TextInput
-                  name="confirmPassword"
-                  label={translate("signup.confirmPassword.label")}
-                  containerClassNames="mb-5"
-                  mode={"flat"}
-                  secureTextEntry={true}
-                  testID={SignupTestIds.confirmPasswordInput}
-                  required
-                />
-
-                <Button
-                  mode="contained"
-                  className="mt-3 mb-10"
-                  onPress={() => onSubmit(props)}
-                  disabled={isEmpty(props.touched) && !props.isValid}
-                  loading={isLoading}
-                  testID={SignupTestIds.submitButton}
-                >
-                  {translate("authScreen.signup.action")}
-                </Button>
-              </>
-            )}
-          </Formik>
-
-          <View className="flex flex-row mx-auto">
-            <Text className="mr-3">{translate("signup.haveAccount.text")}</Text>
-            <Text
-              className="font-bold"
-              style={{ color: theme.colors.primary }}
-              onPress={() => {
-                router.push("/auth/sign-in");
-              }}
-            >
-              {translate("authScreen.signin.action")}
-            </Text>
-          </View>
-        </View>
-
-        <Snackbar
-          className="bg-red-700"
-          duration={3000}
-          visible={isErrorVisible}
-          onDismiss={() => {
-            setIsErrorVisible(false);
+      <SafeAreaView className="flex flex-1">
+        <StatusBar backgroundColor={theme.colors.background} />
+        <ScrollView
+          contentContainerStyle={{
+            display: "flex",
+            flexGrow: 1,
+            justifyContent: "center",
+            backgroundColor: theme.colors.background,
           }}
-          onIconPress={() => setIsErrorVisible(false)}
         >
-          {translate("toaster.failed.genericFailure", {
-            item: translate("authScreen.signup.action"),
-          })}
-        </Snackbar>
-      </ScrollView>
+          <View className="flex flex-col px-8">
+            <Formik
+              initialValues={{}}
+              onSubmit={() => {}}
+              validationSchema={signupValidationSchema(translate)}
+              validateOnBlur
+              validateOnChange={false}
+            >
+              {(props) => (
+                <>
+                  <Text
+                    variant="displaySmall"
+                    className="font-bold"
+                    style={{ color: theme.colors.primary }}
+                  >
+                    {translate("common.appName")}
+                  </Text>
+
+                  <Text className="mt-5 mb-5 text-lg font-bold">
+                    {translate("signup.createAccount.label")}
+                  </Text>
+
+                  <TextInput
+                    name="username"
+                    label={translate("signup.username.label")}
+                    mode={"flat"}
+                    containerClassNames="mb-5"
+                    testID={SignupTestIds.usernameInput}
+                    required
+                  />
+
+                  <TextInput
+                    name="fullName"
+                    label={translate("signup.displayName.label")}
+                    mode={"flat"}
+                    containerClassNames="mb-5"
+                    testID={SignupTestIds.displayNameInput}
+                    required
+                  />
+
+                  <TextInput
+                    name="email"
+                    label={translate("signup.email.label")}
+                    containerClassNames="mb-5"
+                    mode={"flat"}
+                    testID={SignupTestIds.emailInput}
+                    required
+                  />
+
+                  <TextInput
+                    name="password"
+                    label={translate("signup.password.label")}
+                    containerClassNames="mb-5"
+                    mode={"flat"}
+                    secureTextEntry={true}
+                    testID={SignupTestIds.passwordInput}
+                    required
+                  />
+
+                  <TextInput
+                    name="confirmPassword"
+                    label={translate("signup.confirmPassword.label")}
+                    containerClassNames="mb-5"
+                    mode={"flat"}
+                    secureTextEntry={true}
+                    testID={SignupTestIds.confirmPasswordInput}
+                    required
+                  />
+
+                  <Button
+                    mode="contained"
+                    className="mt-3 mb-10"
+                    onPress={() => onSubmit(props)}
+                    disabled={isEmpty(props.touched) && !props.isValid}
+                    loading={isLoading}
+                    testID={SignupTestIds.submitButton}
+                  >
+                    {translate("authScreen.signup.action")}
+                  </Button>
+                </>
+              )}
+            </Formik>
+
+            <View className="flex flex-row mx-auto">
+              <Text className="mr-3">
+                {translate("signup.haveAccount.text")}
+              </Text>
+              <Text
+                className="font-bold"
+                style={{ color: theme.colors.primary }}
+                onPress={() => {
+                  router.push("/auth/sign-in");
+                }}
+              >
+                {translate("authScreen.signin.action")}
+              </Text>
+            </View>
+          </View>
+
+          <Snackbar
+            className="bg-red-700"
+            duration={3000}
+            visible={isErrorVisible}
+            onDismiss={() => {
+              setIsErrorVisible(false);
+            }}
+            onIconPress={() => setIsErrorVisible(false)}
+          >
+            {translate("toaster.failed.genericFailure", {
+              item: translate("authScreen.signup.action"),
+            })}
+          </Snackbar>
+        </ScrollView>
+      </SafeAreaView>
     </>
   );
 };

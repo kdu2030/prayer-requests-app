454e30faf84915c23ab816a55eef24ae8610214b
kdu2030 <kdu2030@gmail.com>
2025-01-19 07:58:02 -0500

    Add Available Prayer Group Colors

--- components/create-prayer-group/create-prayer-group-constants.ts
+++ components/create-prayer-group/create-prayer-group-constants.ts
@@ -8,3 +8,17 @@ export const PRAYER_GROUP_NAME_EXISTS =
   "A prayer group with this name already exists.";
 
 export const NUM_CREATE_PRAYER_GROUP_STEPS = 3;
+
+export const AVAILABLE_PRAYER_GROUP_COLORS = [
+  "#cf1717", // red
+  "#f59802", // orange
+  "#f8fc00", // yellow
+  "#0cc71f", // lime green
+  "#106d20", // green
+  "#11d1ce", // sky blue
+  "#0e39c7", // blue
+  "#5a0c8a", // purple
+  "#bf04b3", // magenta,
+  "#000000", // black,
+  "#ffffff", // white
+];

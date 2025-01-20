d1f491b83836808f4e852adcefca757a6c9d7fe3
kdu2030 <kdu2030@gmail.com>
2025-01-20 15:11:47 -0500

    Migrate to Expo 52 (Draft)

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,17 +1,26 @@
+import {
+  DrawerContentComponentProps,
+  DrawerHeaderProps,
+} from "@react-navigation/drawer";
 import { Drawer } from "expo-router/drawer";
 import * as React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { DrawerContent } from "../../components/navigation/drawer-content";
 import { DrawerHeader } from "../../components/navigation/drawer-header";
 
 const DrawerLayout: React.FC = () => {
   return (
-    <Drawer
-      screenOptions={{
-        header: (props) => <DrawerHeader {...props} />,
-      }}
-      drawerContent={(props) => <DrawerContent {...props} />}
-    />
+    <GestureHandlerRootView>
+      <Drawer
+        screenOptions={{
+          header: (props: DrawerHeaderProps) => <DrawerHeader {...props} />,
+        }}
+        drawerContent={(props: DrawerContentComponentProps) => (
+          <DrawerContent {...props} />
+        )}
+      />
+    </GestureHandlerRootView>
   );
 };
 

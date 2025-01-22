4bfc473399011b7ce1d58df830c97916bf8fc8af
kdu2030 <kdu2030@gmail.com>
2025-01-21 19:16:35 -0500

    Open Image Picker

--- components/create-prayer-group/use-group-image-color-step.ts
+++ components/create-prayer-group/use-group-image-color-step.ts
@@ -1,11 +1,28 @@
+import * as ImagePicker from "expo-image-picker";
 import * as React from "react";
 
 export const useGroupImageColorStep = () => {
   const [isColorPickerModalOpen, setIsColorPickerOpen] =
     React.useState<boolean>(false);
 
+  const selectImage = async () => {
+    try {
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1,
+      });
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return {
     isColorPickerModalOpen,
     setIsColorPickerOpen,
+    selectImage,
   };
 };

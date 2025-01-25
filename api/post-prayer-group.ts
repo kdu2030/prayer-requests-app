6aaf9d66de833920020146d9ddaed0670c02fd6c
kdu2030 <kdu2030@gmail.com>
2025-01-25 17:17:18 -0500

    Add Post Prayer Group

---
+++ api/post-prayer-group.ts
@@ -0,0 +1,49 @@
+import { AxiosInstance } from "axios";
+import * as React from "react";
+
+import { handleApiErrors } from "../helpers/api-helpers";
+import { useApiDataContext } from "../hooks/use-api-data";
+import { ManagedErrorResponse } from "../types/error-handling";
+import { RawPrayerGroupDetails } from "../types/prayer-group-types";
+
+export type PostCreatePrayerGroupRequest = {
+  groupName?: string;
+  description?: string;
+  rules?: string;
+  color?: string;
+  imageFileId?: number;
+};
+
+const postPrayerGroup = async (
+  fetch: AxiosInstance,
+  baseUrl: string,
+  createPrayerGroupRequest: PostCreatePrayerGroupRequest
+): Promise<ManagedErrorResponse<RawPrayerGroupDetails>> => {
+  try {
+    const url = `${baseUrl}/api/prayergroup`;
+
+    const response = await fetch.post<RawPrayerGroupDetails>(
+      url,
+      createPrayerGroupRequest,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return { isError: false, value: response.data };
+  } catch (error) {
+    return handleApiErrors(error);
+  }
+};
+
+export const usePostPrayerGroup = () => {
+  const { fetch, baseUrl } = useApiDataContext();
+
+  return React.useCallback(
+    (createPrayerGroupRequest: PostCreatePrayerGroupRequest) =>
+      postPrayerGroup(fetch, baseUrl, createPrayerGroupRequest),
+    [baseUrl, fetch]
+  );
+};

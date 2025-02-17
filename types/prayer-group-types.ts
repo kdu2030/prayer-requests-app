21a8c1804fcb0683f397879d1880387fbe0b66bd
kdu2030 <kdu2030@gmail.com>
2025-02-17 11:55:16 -0500

    Add Prayer Group Details Types

--- types/prayer-group-types.ts
+++ types/prayer-group-types.ts
@@ -1,10 +1,16 @@
 import { PrayerGroupRole } from "../constants/prayer-group-constants";
-import { RawMediaFile } from "./media-file-types";
+import { MediaFile, RawMediaFile } from "./media-file-types";
 
 export type RawPrayerGroupAdminSummary = {
   id?: number;
   fullName?: string;
   image?: RawMediaFile;
+};
+
+export type PrayerGroupAdminSummary = {
+  userId?: number;
+  fullName?: string;
+  image?: MediaFile;
 };
 
 export type RawPrayerGroupDetails = {
@@ -18,6 +24,19 @@ export type RawPrayerGroupDetails = {
   admins?: RawPrayerGroupAdminSummary[];
   imageFile?: RawMediaFile;
   bannerImageFile?: RawMediaFile;
+};
+
+export type PrayerGroupDetails = {
+  prayerGroupId?: number;
+  groupName?: string;
+  description?: string;
+  rules?: string;
+  color?: string;
+  isUserJoined?: boolean;
+  userRole?: PrayerGroupRole;
+  admins?: PrayerGroupAdminSummary[];
+  imageFile?: MediaFile;
+  bannerImageFile?: MediaFile;
 };
 
 export type RawPrayerGroupSummary = {

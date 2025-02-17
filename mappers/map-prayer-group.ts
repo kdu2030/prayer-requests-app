74e2ec5cffd3669e93e75749503f76d3d61ca412
kdu2030 <kdu2030@gmail.com>
2025-02-17 11:59:59 -0500

    Add Mappers

--- mappers/map-prayer-group.ts
+++ mappers/map-prayer-group.ts
@@ -1,7 +1,11 @@
 import { PostCreatePrayerGroupRequest } from "../api/post-prayer-group";
 import { CreatePrayerGroupForm } from "../components/create-prayer-group/create-prayer-group-types";
 import {
+  PrayerGroupAdminSummary,
+  PrayerGroupDetails,
   PrayerGroupSummary,
+  RawPrayerGroupAdminSummary,
+  RawPrayerGroupDetails,
   RawPrayerGroupSummary,
 } from "../types/prayer-group-types";
 import { mapMediaFile } from "./map-media-file";
@@ -32,5 +36,36 @@ export const mapPrayerGroupSummary = (
     ...rawPrayerGroupSummary,
     prayerGroupId: rawPrayerGroupSummary.id,
     imageFile: mapMediaFile(rawPrayerGroupSummary.imageFile),
+  };
+};
+
+export const mapPrayerGroupAdmin = (
+  rawPrayerGroupAdmin: RawPrayerGroupAdminSummary
+): PrayerGroupAdminSummary => {
+  return {
+    userId: rawPrayerGroupAdmin.id,
+    image: mapMediaFile(rawPrayerGroupAdmin.image),
+    fullName: rawPrayerGroupAdmin.fullName,
+  };
+};
+
+export const mapPrayerGroupDetails = (
+  rawPrayerGroupDetails: RawPrayerGroupDetails
+): PrayerGroupDetails => {
+  const admins = rawPrayerGroupDetails.admins?.map((admin) =>
+    mapPrayerGroupAdmin(admin)
+  );
+
+  return {
+    prayerGroupId: rawPrayerGroupDetails.id,
+    description: rawPrayerGroupDetails.description,
+    groupName: rawPrayerGroupDetails.groupName,
+    color: rawPrayerGroupDetails.color,
+    rules: rawPrayerGroupDetails.rules,
+    isUserJoined: rawPrayerGroupDetails.isUserJoined,
+    userRole: rawPrayerGroupDetails.userRole,
+    admins,
+    imageFile: mapMediaFile(rawPrayerGroupDetails.imageFile),
+    bannerImageFile: mapMediaFile(rawPrayerGroupDetails.bannerImageFile),
   };
 };

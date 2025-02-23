b4879118eb56a6f4352df6b01d474d00f7156605
kdu2030 <kdu2030@gmail.com>
2025-02-22 23:09:15 -0500

    Fix Stale Prayer Group Issue

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -20,7 +20,12 @@ const DrawerLayout: React.FC = () => {
           <DrawerContent {...props} />
         )}
         backBehavior="history"
-      />
+      >
+        <Drawer.Screen
+          name="prayergroup/[id]"
+          options={{ unmountOnBlur: true }}
+        />
+      </Drawer>
     </GestureHandlerRootView>
   );
 };

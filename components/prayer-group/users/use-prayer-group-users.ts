1d12435f3da3e3bd2cab9c86f8c46f708e7313b5
kdu2030 <kdu2030@gmail.com>
2025-02-26 18:52:41 -0500

    Load Users

--- components/prayer-group/users/use-prayer-group-users.ts
+++ components/prayer-group/users/use-prayer-group-users.ts
@@ -1,5 +1,8 @@
 import * as React from "react";
 
+import { useGetPrayerGroupUsers } from "../../../api/get-prayer-group-users";
+import { PrayerGroupRole } from "../../../constants/prayer-group-constants";
+import { mapPrayerGroupUser } from "../../../mappers/map-prayer-group";
 import { PrayerGroupUserSummary } from "../../../types/prayer-group-types";
 
 export const usePrayerGroupUsers = (prayerGroupId: number) => {
@@ -8,7 +11,33 @@ export const usePrayerGroupUsers = (prayerGroupId: number) => {
   >([]);
 
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
-  const [isError, setIsError] = React.useState<boolean>(true);
+  const [isError, setIsError] = React.useState<boolean>(false);
 
-  return { isLoading, setIsLoading, isError };
+  const getPrayerGroupUsers = useGetPrayerGroupUsers();
+
+  const loadPrayerGroupUsers = React.useCallback(async () => {
+    setIsLoading(true);
+    const response = await getPrayerGroupUsers(prayerGroupId, [
+      PrayerGroupRole.Member,
+      PrayerGroupRole.Admin,
+    ]);
+    setIsLoading(false);
+
+    if (response.isError) {
+      setIsError(true);
+      return;
+    }
+
+    const responseUsers = response.value.users.map((user) =>
+      mapPrayerGroupUser(user)
+    );
+    setPrayerGroupUsers(responseUsers);
+    console.log(responseUsers);
+  }, [getPrayerGroupUsers, prayerGroupId]);
+
+  React.useEffect(() => {
+    loadPrayerGroupUsers();
+  }, [loadPrayerGroupUsers]);
+
+  return { isLoading, setIsLoading, isError, prayerGroupUsers };
 };

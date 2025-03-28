dce67b37fa5f467f9c3712d4604972d6cf7a367e
kdu2030 <kdu2030@gmail.com>
2025-03-28 19:55:57 -0400

    Add Test IDs to Group Banner

--- components/prayer-group/header/prayer-group-banner.tsx
+++ components/prayer-group/header/prayer-group-banner.tsx
@@ -4,6 +4,7 @@ import { Image, View } from "react-native";
 import { Text } from "react-native-paper";
 
 import { useI18N } from "../../../hooks/use-i18n";
+import { PrayerGroupHeaderTestIds } from "./tests/test-ids";
 
 type Props = {
   uri: string | undefined;
@@ -21,9 +22,13 @@ export const PrayerGroupBanner: React.FC<Props> = ({ uri }) => {
           style={{
             resizeMode: "cover",
           }}
+          testID={PrayerGroupHeaderTestIds.imageBanner}
         />
       ) : (
-        <View className="w-full h-20 justify-center items-center">
+        <View
+          className="w-full h-20 justify-center items-center"
+          testID={PrayerGroupHeaderTestIds.bannerPlaceholder}
+        >
           <MaterialIcons name="image" size={36} color="#1f2937" />
           <Text variant="bodyMedium" className="text-gray-800 mt-1">
             {translate("image.missing.label")}
---
+++ components/prayer-group/header/tests/test-ids.ts
@@ -0,0 +1,4 @@
+export enum PrayerGroupHeaderTestIds {
+  imageBanner = "prayer-group-header-image-banner",
+  bannerPlaceholder = "prayer-group-header-banner-placeholder",
+}

10ed3b49fdadfb88df25c3d1ce36232c99ea79f6
kdu2030 <kdu2030@gmail.com>
2025-03-28 13:32:57 -0400

    Add Go Back to Group Error

--- components/prayer-group/error-screens/user-permission-error.tsx
+++ components/prayer-group/error-screens/user-permission-error.tsx
@@ -1,4 +1,5 @@
 import { MaterialIcons } from "@expo/vector-icons";
+import { Href, router } from "expo-router";
 import * as React from "react";
 import { View } from "react-native";
 import { Button, useTheme } from "react-native-paper";
@@ -6,16 +7,24 @@ import { Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { useI18N } from "../../../hooks/use-i18n";
-import { ErrorScreen } from "../../layouts/error-screen";
+import { usePrayerGroupContext } from "../prayer-group-context";
 import { PrayerGroupSectionHeader } from "../section-header/prayer-group-section-header";
 
-type Props = {
-  onRetry: () => void;
-};
-
-export const PrayerGroupUsersError: React.FC<Props> = ({ onRetry }) => {
+export const PrayerGroupUsersError: React.FC = () => {
   const { translate } = useI18N();
   const theme = useTheme();
+  const { prayerGroupDetails } = usePrayerGroupContext();
+
+  const goBackToGroup = () => {
+    if (!prayerGroupDetails?.prayerGroupId) {
+      return;
+    }
+
+    router.push({
+      pathname: "/prayergroup/[id]",
+      params: { id: prayerGroupDetails.prayerGroupId },
+    } as Href<any>);
+  };
 
   return (
     <SafeAreaView className="flex-1">
@@ -33,7 +42,7 @@ export const PrayerGroupUsersError: React.FC<Props> = ({ onRetry }) => {
           {translate("prayerGroup.permissionError.message")}
         </Text>
 
-        <Button mode="contained" className="mt-5" onPress={onRetry}>
+        <Button mode="contained" className="mt-5" onPress={goBackToGroup}>
           {translate("prayerGroup.permissionError.goBackToGroup")}
         </Button>
       </View>

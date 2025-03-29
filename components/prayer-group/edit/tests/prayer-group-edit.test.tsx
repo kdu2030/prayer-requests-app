4f16f6675fcfbff45885907b63dacb684e948273
kdu2030 <kdu2030@gmail.com>
2025-03-29 10:23:34 -0400

    Add Group Name Test

--- components/prayer-group/edit/tests/prayer-group-edit.test.tsx
+++ components/prayer-group/edit/tests/prayer-group-edit.test.tsx
@@ -1,10 +1,14 @@
-import { RenderResult } from "@testing-library/react-native";
+import "@testing-library/jest-native/extend-expect";
+import "@testing-library/jest-native";
+
+import { fireEvent, RenderResult } from "@testing-library/react-native";
 
 import { mapPrayerGroupDetails } from "../../../../mappers/map-prayer-group";
 import { mountComponent } from "../../../../tests/utils/test-utils";
 import { PrayerGroupDetails } from "../../../../types/prayer-group-types";
 import { mockRawPrayerGroupDetails, mockUserData } from "../../tests/mock-data";
 import { PrayerGroupEdit } from "../prayer-group-edit";
+import { PrayerGroupEditTestIds } from "./test-ids";
 
 let component: RenderResult;
 
@@ -47,5 +51,24 @@ describe(PrayerGroupEdit, () => {
     );
 
     expect(component).toBeTruthy();
+  });
+
+  test("Updating group name will update the group name in the preview", async () => {
+    component = mountPrayerGroupEdit(
+      mapPrayerGroupDetails(mockRawPrayerGroupDetails)
+    );
+
+    const newGroupName = "Ravenclaw";
+
+    const groupNameTextInput = component.getByTestId(
+      PrayerGroupEditTestIds.groupNameInput
+    );
+    fireEvent.changeText(groupNameTextInput, newGroupName);
+    fireEvent(groupNameTextInput, "blur");
+
+    const groupNamePreview = await component.findByTestId(
+      PrayerGroupEditTestIds.groupPreviewName
+    );
+    expect(groupNamePreview).toHaveTextContent(newGroupName);
   });
 });

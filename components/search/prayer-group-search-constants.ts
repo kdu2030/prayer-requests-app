d39d8bbea2c6888cf8e3acd8260f0ca3c3dd8be9
kdu2030 <kdu2030@gmail.com>
2025-04-01 22:11:51 -0400

    Add Search Prayer Group Summaries

---
+++ components/search/prayer-group-search-constants.ts
@@ -0,0 +1,3 @@
+export const MAX_RESULT_COUNT = 10;
+
+export const DEBOUNCE_TIME = 300;
--- components/search/use-prayer-group-search.ts
+++ components/search/use-prayer-group-search.ts
@@ -1,8 +1,15 @@
+import { compact, debounce } from "lodash";
 import * as React from "react";
 
+import { useGetPrayerGroupsBySearch } from "../../api/get-prayer-groups-by-search";
 import { SEARCH_MIN_CHARACTERS } from "../../constants/input-constants";
 import { useI18N } from "../../hooks/use-i18n";
+import { mapPrayerGroupSummary } from "../../mappers/map-prayer-group";
 import { PrayerGroupSummary } from "../../types/prayer-group-types";
+import {
+  DEBOUNCE_TIME,
+  MAX_RESULT_COUNT,
+} from "./prayer-group-search-constants";
 
 export const usePrayerGroupSearch = () => {
   const { translate } = useI18N();
@@ -11,6 +18,8 @@ export const usePrayerGroupSearch = () => {
   const [groupSearchResults, setGroupSearchResults] = React.useState<
     PrayerGroupSummary[]
   >([]);
+
+  const getPrayerGroupsBySearch = useGetPrayerGroupsBySearch();
 
   const placeholderMessage = React.useMemo(() => {
     if (groupQuery.length < SEARCH_MIN_CHARACTERS) {
@@ -22,13 +31,40 @@ export const usePrayerGroupSearch = () => {
     }
   }, [groupQuery.length, groupSearchResults.length, translate]);
 
+  const loadPrayerGroups = async (query: string) => {
+    const response = await getPrayerGroupsBySearch(query, MAX_RESULT_COUNT);
+    if (response.isError) {
+      return;
+    }
+
+    const prayerGroupSummaries = response.value.map((rawSummary) =>
+      mapPrayerGroupSummary(rawSummary)
+    );
+    setGroupSearchResults(compact(prayerGroupSummaries));
+
+    // FIXME: Remove, for testing only
+    console.log(prayerGroupSummaries);
+  };
+
   const onChangeQuery = (query: string) => {
     setGroupQuery(query);
+
+    if (query.length < SEARCH_MIN_CHARACTERS) {
+      setGroupSearchResults([]);
+      return;
+    }
+
+    const debouncedLoadResults = debounce(
+      () => loadPrayerGroups(query),
+      DEBOUNCE_TIME
+    );
+    debouncedLoadResults();
   };
 
   return {
     groupQuery,
     onChangeQuery,
     placeholderMessage,
+    groupSearchResults,
   };
 };

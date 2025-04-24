756a208ee8c690cab4ab0834bc38791f46756d6b
kdu2030 <kdu2030@gmail.com>
2025-04-23 22:11:35 -0400

    Set up RequestBodyStep

---
+++ components/prayer-group/create-prayer-request/request-body-step/tests/request-body-step.test.tsx
@@ -0,0 +1,48 @@
+import { renderHook, RenderResult } from "@testing-library/react-native";
+import { Formik } from "formik";
+import * as React from "react";
+
+import { mountComponent } from "../../../../../tests/utils/test-utils";
+import { TranslationKey } from "../../../../../types/languages";
+import { CreatePrayerRequestForm } from "../../create-prayer-request-types";
+import { RequestBodyStep } from "../request-body-step";
+import { useRequestBodyValidationSchema } from "../use-request-body-validation-schema";
+
+let component: RenderResult;
+
+const mockTranslate = jest.fn();
+
+jest.mock("../../../../../hooks/use-i18n", () => ({
+  i18n: {
+    language: "en-US",
+  },
+  translate: (key: TranslationKey, args: object) => mockTranslate(key, args),
+}));
+
+const mountRequestBody = (formValues?: CreatePrayerRequestForm) => {
+  const { result: requestBodyValidationSchemaRef } = renderHook(() =>
+    useRequestBodyValidationSchema()
+  );
+
+  return mountComponent(
+    <Formik
+      initialValues={formValues ?? {}}
+      validationSchema={requestBodyValidationSchemaRef.current}
+      onSubmit={() => {}}
+    >
+      <RequestBodyStep setWizardStep={() => {}} />
+    </Formik>
+  );
+};
+
+describe(RequestBodyStep, () => {
+  afterEach(() => {
+    component.unmount();
+    jest.resetAllMocks();
+  });
+
+  test("Mount test", () => {
+    component = mountRequestBody();
+    expect(component).toBeTruthy();
+  });
+});

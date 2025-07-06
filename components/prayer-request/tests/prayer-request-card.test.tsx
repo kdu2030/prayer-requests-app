7363f5cdc37c14219a0347df582fbc04bdc4778e
kdu2030 <kdu2030@gmail.com>
2025-07-05 20:14:23 -0400

    Add Like Button

--- components/prayer-request/tests/prayer-request-card.test.tsx
+++ components/prayer-request/tests/prayer-request-card.test.tsx
@@ -1,6 +1,12 @@
 import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock";
-import { RenderResult } from "@testing-library/react-native";
+import {
+  fireEvent,
+  RenderResult,
+  waitFor,
+} from "@testing-library/react-native";
 
+import { getArrayTestId } from "../../../helpers/utils";
+import { mapPrayerRequests } from "../../../mappers/map-prayer-request";
 import { mountComponent } from "../../../tests/utils/test-utils";
 import { PrayerRequestModel } from "../../../types/prayer-request-types";
 import {
@@ -8,6 +14,7 @@ import {
   mockUserData,
 } from "../../prayer-group/tests/mock-data";
 import { PrayerRequestCard } from "../prayer-request-card";
+import { PrayerRequestCardTestIds } from "./test-ids";
 
 let component: RenderResult;
 
@@ -50,13 +57,41 @@ const mountPrayerRequestCard = (
 describe(PrayerRequestCard, () => {
   afterEach(() => {
     component?.unmount();
+    jest.resetAllMocks();
   });
 
   test("Mount test", () => {
-    component = mountPrayerRequestCard(
-      mockPrayerRequests[0],
-      mockPrayerRequests
-    );
+    const prayerRequests = mapPrayerRequests(mockPrayerRequests);
+
+    component = mountPrayerRequestCard(prayerRequests[0], prayerRequests);
     expect(component).toBeTruthy();
+  });
+
+  test("Like button Adds prayer request like if user hasn't liked prayer request", async () => {
+    let updatedPrayerRequests: PrayerRequestModel[] = [];
+
+    mockSetPrayerRequests.mockImplementation(
+      (prayerRequests: PrayerRequestModel[]) => {
+        updatedPrayerRequests = prayerRequests;
+      }
+    );
+
+    mockPostPrayerRequestLike.mockReturnValue({ isError: false });
+
+    const prayerRequests = mapPrayerRequests(mockPrayerRequests);
+
+    component = mountPrayerRequestCard(prayerRequests[0], prayerRequests);
+
+    const likeButton = component.getByTestId(
+      getArrayTestId(PrayerRequestCardTestIds.likeButton, 9)
+    );
+    fireEvent.press(likeButton);
+
+    await waitFor(() => {
+      expect(mockPostPrayerRequestLike).toHaveBeenCalledWith(1, 9);
+
+      expect(updatedPrayerRequests[0].isUserLiked).toBeTruthy();
+      expect(updatedPrayerRequests[0].likeCount).toBe(2);
+    });
   });
 });

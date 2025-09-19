461e1b2f47deba23962445f924accac52a38777f
kdu2030 <kdu2030@gmail.com>
2025-09-18 21:54:39 -0400

    Add Post Prayer Group Users Search Client

--- api/post-prayer-group-users-search.ts
+++ api/post-prayer-group-users-search.ts
@@ -1,14 +1,60 @@
+import * as React from "react";
 import { AxiosInstance } from "axios";
-import { PrayerGroupRole } from "../constants/prayer-group-constants"
+import { PrayerGroupRole } from "../constants/prayer-group-constants";
 import { SortConfig } from "../types/api-response-types";
 import { PrayerGroupUserSummary } from "../types/prayer-group-types";
+import { ManagedErrorResponse } from "../types/error-handling";
+import { handleApiErrors } from "../helpers/api-helpers";
+import { useApiDataContext } from "../hooks/use-api-data";
 
 export type GetPrayerGroupUsersRequest = {
-    prayerGroupRoles?: PrayerGroupRole[];
-    sortConfig: SortConfig
-}
+  prayerGroupRoles?: PrayerGroupRole[];
+  sortConfig: SortConfig;
+};
 
 export type GetPrayerGroupUsersResponse = {
-    prayerGroupUsers: PrayerGroupUserSummary[]
-}
+  prayerGroupUsers: PrayerGroupUserSummary[];
+};
 
+const postPrayerGroupUsersSearch = async (
+  fetch: AxiosInstance,
+  baseUrl: string,
+  prayerGroupId: number,
+  prayerGroupUsersRequest: GetPrayerGroupUsersRequest
+): Promise<ManagedErrorResponse<GetPrayerGroupUsersResponse>> => {
+  const url = `${baseUrl}/api/prayergroup/${prayerGroupId}/users`;
+
+  try {
+    const response = await fetch.post<GetPrayerGroupUsersResponse>(
+      url,
+      prayerGroupUsersRequest,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return { isError: false, value: response.data };
+  } catch (error) {
+    return handleApiErrors(error);
+  }
+};
+
+export const usePostGroupUsersSearch = () => {
+  const { fetch, baseUrl } = useApiDataContext();
+
+  return React.useCallback(
+    (
+      prayerGroupId: number,
+      prayerGroupUsersRequest: GetPrayerGroupUsersRequest
+    ) =>
+      postPrayerGroupUsersSearch(
+        fetch,
+        baseUrl,
+        prayerGroupId,
+        prayerGroupUsersRequest
+      ),
+    []
+  );
+};

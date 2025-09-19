cac91cfa0eeaaf69beeb121060b87a4dcc5556eb
kdu2030 <kdu2030@gmail.com>
2025-09-18 22:22:13 -0400

    Add Use Post Prayer Group User

---
+++ api/post-prayer-group-user.ts
@@ -0,0 +1,40 @@
+import { AxiosInstance } from "axios";
+import * as React from "react";
+import { MediaFile } from "../types/media-file-types";
+import { PrayerGroupRole } from "../constants/prayer-group-constants";
+import { handleApiErrors } from "../helpers/api-helpers";
+import { ManagedErrorResponse } from "../types/error-handling";
+import { useApiDataContext } from "../hooks/use-api-data";
+
+export type PostPrayerGroupUserResponse = {
+  userId?: number;
+  fullName?: string;
+  prayerGroupRole?: PrayerGroupRole;
+};
+
+const postPrayerGroupUser = async (
+  fetch: AxiosInstance,
+  baseUrl: string,
+  prayerGroupId: number,
+  userId: number
+): Promise<ManagedErrorResponse<PostPrayerGroupUserResponse>> => {
+  const url = `${baseUrl}/api/prayergroup/${prayerGroupId}/user/${userId}`;
+
+  try {
+    const response = await fetch.post<PostPrayerGroupUserResponse>(url);
+
+    return { isError: false, value: response.data };
+  } catch (error) {
+    return handleApiErrors(error);
+  }
+};
+
+export const usePostPrayerGroupUser = () => {
+  const { baseUrl, fetch } = useApiDataContext();
+
+  return React.useCallback(
+    (prayerGroupId: number, userId: number) =>
+      postPrayerGroupUser(fetch, baseUrl, prayerGroupId, userId),
+    []
+  );
+};

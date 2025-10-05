63b6ffc1d36ca61d6006bf7689b809931262b0b7
kdu2030 <kdu2030@gmail.com>
2025-10-04 20:48:15 -0400

    Add Toaster Snackbar

---
+++ components/toasters/toaster-snackbar.tsx
@@ -0,0 +1,45 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import * as React from "react";
+import { View } from "react-native";
+import { Snackbar, Text, useTheme } from "react-native-paper";
+import { ToasterConfig } from "./toaster-type";
+import { useToasterContext } from "./toaster-context";
+
+type Props = {
+  toaster: ToasterConfig;
+};
+
+export const ToasterSnackbar: React.FC<Props> = ({ toaster }) => {
+  const theme = useTheme();
+  const [isVisible, setIsVisible] = React.useState<boolean>(true);
+  const { removeToaster } = useToasterContext();
+
+  const onDismissToaster = () => {
+    setIsVisible(false);
+    setTimeout(() => removeToaster(toaster.toasterId), 3000);
+  };
+
+  return (
+    <Snackbar
+      style={{
+        backgroundColor:
+          toaster.variant === "success"
+            ? theme.colors.primary
+            : theme.colors.error,
+      }}
+      duration={3000}
+      visible={isVisible}
+      onDismiss={onDismissToaster}
+      onIconPress={onDismissToaster}
+    >
+      <View className="flex flex-row items-center">
+        <MaterialIcons
+          size={24}
+          name={toaster.variant === "success" ? "check" : "warning"}
+          color={"white"}
+        />
+        <Text className="ml-2 text-white">{toaster.message}</Text>
+      </View>
+    </Snackbar>
+  );
+};

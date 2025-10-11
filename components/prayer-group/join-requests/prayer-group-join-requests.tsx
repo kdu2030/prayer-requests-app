d451390255c5430fb102b4f43735f8a65e010ba9
= <kdu2030@gmail.com>
2025-10-11 19:40:31 -0400

    Add Error Screen

--- components/prayer-group/join-requests/prayer-group-join-requests.tsx
+++ components/prayer-group/join-requests/prayer-group-join-requests.tsx
@@ -3,6 +3,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { useI18N } from "../../../hooks/use-i18n";
 import { LoadStatus } from "../../../types/api-response-types";
+import { ErrorScreen } from "../../layouts/error-screen";
 import { SpinnerScreen } from "../../layouts/spinner-screen";
 import { PrayerGroupSectionHeader } from "../section-header/prayer-group-section-header";
 import { usePrayerGroupJoinRequests } from "./use-prayer-group-join-requests";
@@ -14,7 +15,8 @@ type Props = {
 export const PrayerGroupJoinRequests: React.FC<Props> = ({ prayerGroupId }) => {
   const { translate } = useI18N();
 
-  const { joinRequestLoadStatus } = usePrayerGroupJoinRequests(prayerGroupId);
+  const { joinRequestLoadStatus, loadJoinRequests } =
+    usePrayerGroupJoinRequests(prayerGroupId);
 
   return (
     <SafeAreaView className="flex-1">
@@ -28,6 +30,13 @@ export const PrayerGroupJoinRequests: React.FC<Props> = ({ prayerGroupId }) => {
             "prayerGroup.joinRequest.loadingJoinRequests"
           )}
           showSafeArea={false}
+        />
+      )}
+
+      {joinRequestLoadStatus === LoadStatus.Error && (
+        <ErrorScreen
+          errorLabel={translate("prayerGroup.joinRequest.unableToLoad")}
+          onRetry={loadJoinRequests}
         />
       )}
     </SafeAreaView>
--- components/prayer-group/join-requests/use-prayer-group-join-requests.ts
+++ components/prayer-group/join-requests/use-prayer-group-join-requests.ts
@@ -1,16 +1,11 @@
 import * as React from "react";
 
 import { usePostJoinRequestsSearch } from "../../../api/post-join-requests-search";
-import { useI18N } from "../../../hooks/use-i18n";
 import { LoadStatus } from "../../../types/api-response-types";
 import { JoinRequestModel } from "../../../types/join-request-types";
-import { useToasterContext } from "../../toasters/toaster-context";
 import { JOIN_REQUEST_SORT_CONFIG } from "./join-request-constants";
 
 export const usePrayerGroupJoinRequests = (prayerGroupId: number) => {
-  const { translate } = useI18N();
-  const { openToaster } = useToasterContext();
-
   const [joinRequests, setJoinRequests] = React.useState<JoinRequestModel[]>(
     []
   );
@@ -28,22 +23,17 @@ export const usePrayerGroupJoinRequests = (prayerGroupId: number) => {
     );
 
     if (response.isError) {
-      openToaster({
-        message: translate("prayerGroup.joinRequest.unableToLoad"),
-        variant: "error",
-      });
       setJoinRequestLoadStatus(LoadStatus.Error);
-
       return;
     }
 
     setJoinRequests(response.value.joinRequests ?? []);
     setJoinRequestLoadStatus(LoadStatus.Success);
-  }, [openToaster, prayerGroupId, postJoinRequestsSearch, translate]);
+  }, [prayerGroupId, postJoinRequestsSearch]);
 
   React.useEffect(() => {
     loadJoinRequests();
   }, [loadJoinRequests, prayerGroupId]);
 
-  return { joinRequests, joinRequestLoadStatus };
+  return { joinRequests, joinRequestLoadStatus, loadJoinRequests };
 };

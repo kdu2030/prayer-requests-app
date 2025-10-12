cb00a41a13b3142fb49856d2b4635c790d1852c8
= <kdu2030@gmail.com>
2025-10-11 20:57:59 -0400

    Adjust Styling

--- components/prayer-group/join-requests/join-request-actions.tsx
+++ components/prayer-group/join-requests/join-request-actions.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { View } from "react-native";
-import { Button } from "react-native-paper";
+import { Button, useTheme } from "react-native-paper";
 
 import { useI18N } from "../../../hooks/use-i18n";
 
 export const JoinRequestActions: React.FC = () => {
   const { translate } = useI18N();
+  const theme = useTheme();
 
   return (
     <View className="flex flex-row items-center gap-x-1 flex-1">
@@ -16,7 +17,12 @@ export const JoinRequestActions: React.FC = () => {
       </View>
 
       <View className="flex-1">
-        <Button mode="text" icon={"close"}>
+        <Button
+          mode="text"
+          icon={"close"}
+          textColor={theme.colors.error}
+          rippleColor={theme.colors.errorContainer}
+        >
           {translate("common.actions.reject")}
         </Button>
       </View>

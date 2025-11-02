0ff67e21a80fbe3f654c2a220fc909c4492666f4
kdu2030 <kdu2030@gmail.com>
2025-11-02 12:25:03 -0500

    Add Prayer Request Context Provider to Layout

--- app/(drawer)/prayergroup/[id]/_layout.tsx
+++ app/(drawer)/prayergroup/[id]/_layout.tsx
@@ -2,18 +2,21 @@ import { Drawer } from "expo-router/drawer";
 import * as React from "react";
 
 import { PrayerGroupContextProvider } from "../../../../components/prayer-group/prayer-group-context";
+import { PrayerRequestContextProvider } from "../../../../components/prayer-request/prayer-request-context";
 
 const PrayerGroupLayout: React.FC = () => {
   return (
     <PrayerGroupContextProvider>
-      <Drawer screenOptions={{ headerShown: false }} backBehavior="history">
-        <Drawer.Screen name="users" options={{ unmountOnBlur: true }} />
-        <Drawer.Screen name="create" />
-        <Drawer.Screen name="join-requests" />
-        <Drawer.Screen name="edit" />
-        <Drawer.Screen name="about" />
-        <Drawer.Screen name="index" />
-      </Drawer>
+      <PrayerRequestContextProvider>
+        <Drawer screenOptions={{ headerShown: false }} backBehavior="history">
+          <Drawer.Screen name="users" options={{ unmountOnBlur: true }} />
+          <Drawer.Screen name="create" />
+          <Drawer.Screen name="join-requests" />
+          <Drawer.Screen name="edit" />
+          <Drawer.Screen name="about" />
+          <Drawer.Screen name="index" />
+        </Drawer>
+      </PrayerRequestContextProvider>
     </PrayerGroupContextProvider>
   );
 };

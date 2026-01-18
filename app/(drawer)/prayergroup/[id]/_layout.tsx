4cb0057dbe641267b0f4225d12244d9427185dcb
kdu2030 <kdu2030@gmail.com>
2026-01-18 17:17:15 -0500

    Replace Nested Drawer with a Stack

--- app/(drawer)/prayergroup/[id]/_layout.tsx
+++ app/(drawer)/prayergroup/[id]/_layout.tsx
@@ -1,4 +1,4 @@
-import { Drawer } from "expo-router/drawer";
+import { Stack } from "expo-router";
 import * as React from "react";
 
 import { PrayerGroupContextProvider } from "../../../../components/prayer-group/prayer-group-context";
@@ -8,14 +8,14 @@ const PrayerGroupLayout: React.FC = () => {
   return (
     <PrayerGroupContextProvider>
       <PrayerRequestContextProvider>
-        <Drawer screenOptions={{ headerShown: false }} backBehavior="history">
-          <Drawer.Screen name="users" options={{ popToTopOnBlur: true }} />
-          <Drawer.Screen name="create" />
-          <Drawer.Screen name="join-requests" />
-          <Drawer.Screen name="edit" />
-          <Drawer.Screen name="about" />
-          <Drawer.Screen name="index" />
-        </Drawer>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="users" />
+          <Stack.Screen name="create" />
+          <Stack.Screen name="join-requests" />
+          <Stack.Screen name="edit" />
+          <Stack.Screen name="about" />
+          <Stack.Screen name="index" />
+        </Stack>
       </PrayerRequestContextProvider>
     </PrayerGroupContextProvider>
   );

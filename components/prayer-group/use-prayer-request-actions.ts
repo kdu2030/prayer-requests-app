0d15dbe643a456ac0245f10caf57680f3eb43715
kdu2030 <kdu2030@gmail.com>
2026-01-18 20:58:32 -0500

    Add Add Prayer Request Bookmark Handling

--- components/prayer-group/use-prayer-request-actions.ts
+++ components/prayer-group/use-prayer-request-actions.ts
@@ -3,8 +3,13 @@ import * as React from "react";
 
 import { useDeletePrayerRequestBookmark } from "../../api/delete-prayer-request-bookmark";
 import { usePostPrayerRequestBookmark } from "../../api/post-prayer-request-bookmark";
+import { useApiDataContext } from "../../hooks/use-api-data";
 import { useI18N } from "../../hooks/use-i18n";
-import { PrayerRequestModel } from "../../types/prayer-request-types";
+import {
+  PrayerRequestActionCreateRequest,
+  PrayerRequestModel,
+} from "../../types/prayer-request-types";
+import { useToasterContext } from "../toasters/toaster-context";
 
 export const usePrayerRequestActions = () => {
   const { translate } = useI18N();
@@ -16,8 +21,12 @@ export const usePrayerRequestActions = () => {
   const [isToggleBookmarkLoading, setIsToggleBookmarkLoading] =
     React.useState<boolean>(false);
 
+  const { openToaster } = useToasterContext();
+
   const postPrayerRequestBookmark = usePostPrayerRequestBookmark();
   const deletePrayerRequestBookmark = useDeletePrayerRequestBookmark();
+
+  const { userData } = useApiDataContext();
 
   const openPrayerRequestActions = (prayerRequest: PrayerRequestModel) => {
     if (!prayerRequestActionsRef.current) {
@@ -31,6 +40,37 @@ export const usePrayerRequestActions = () => {
   const closePrayerRequestActions = () => {
     prayerRequestActionsRef.current?.close();
     setSelectedPrayerRequest(undefined);
+  };
+
+  const addPrayerRequestBookmark = async (prayerRequestId: number) => {
+    if (!userData?.userId) {
+      return;
+    }
+
+    const createRequest: PrayerRequestActionCreateRequest = {
+      userId: userData.userId,
+      submittedDate: new Date().toISOString(),
+    };
+
+    const response = await postPrayerRequestBookmark(
+      prayerRequestId,
+      createRequest,
+    );
+
+    if (response.isError) {
+      openToaster({
+        message: translate("toaster.savePrayerRequest.failure"),
+        variant: "error",
+      });
+      return;
+    }
+
+    closePrayerRequestActions();
+
+    openToaster({
+      message: translate("toaster.savePrayerRequest.success"),
+      variant: "error",
+    });
   };
 
   const toggleBookmark = async () => {

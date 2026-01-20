a3175023818f5b051054359d74e536a46025a840
kdu2030 <kdu2030@gmail.com>
2026-01-19 19:00:59 -0500

    Add Prayer Request Skeleton

--- components/prayer-request/prayer-request-card.tsx
+++ components/prayer-request/prayer-request-card.tsx
@@ -8,6 +8,7 @@ import { useI18N } from "../../hooks/use-i18n";
 import { CultureCode } from "../../types/languages";
 import { PrayerRequestModel } from "../../types/prayer-request-types";
 import { ProfilePicture } from "../layouts/profile-picture";
+import { PrayerRequestSkeleton } from "./prayer-request-skeleton";
 import { PrayerRequestCardTestIds } from "./tests/test-ids";
 import { usePrayerRequestCard } from "./use-prayer-request-card";
 
@@ -37,123 +38,127 @@ export const PrayerRequestCard: React.FC<Props> = ({
   );
 
   return (
-    <View
-      className="p-5 border-b"
-      style={{
-        backgroundColor: theme.colors.background,
-        borderColor: theme.colors.outline,
-      }}
-    >
-      <View className="flex flex-row items-center">
-        {displayUser && (
-          <View className="flex flex-row items-center">
-            <ProfilePicture
-              url={prayerRequest.user?.image?.fileUrl}
-              width={24}
-              height={24}
+    <>
+      <View
+        className="p-5 border-b"
+        style={{
+          backgroundColor: theme.colors.background,
+          borderColor: theme.colors.outline,
+        }}
+      >
+        <View className="flex flex-row items-center">
+          {displayUser && (
+            <View className="flex flex-row items-center">
+              <ProfilePicture
+                url={prayerRequest.user?.image?.fileUrl}
+                width={24}
+                height={24}
+                testID={getArrayTestId(
+                  PrayerRequestCardTestIds.userProfilePicture,
+                  prayerRequest.prayerRequestId,
+                )}
+              />
+              <Text className="ml-2" variant="bodyMedium">
+                {prayerRequest.user?.fullName}
+              </Text>
+            </View>
+          )}
+
+          {prayerRequest.createdDate && (
+            <Text
+              variant="bodyMedium"
+              className="ml-4 text-gray-500"
               testID={getArrayTestId(
-                PrayerRequestCardTestIds.userProfilePicture,
+                PrayerRequestCardTestIds.createdDate,
                 prayerRequest.prayerRequestId,
               )}
-            />
-            <Text className="ml-2" variant="bodyMedium">
-              {prayerRequest.user?.fullName}
+            >
+              {formatDate(
+                prayerRequest.createdDate,
+                i18n.language as CultureCode,
+              )}
             </Text>
+          )}
+        </View>
+
+        <Text
+          variant="titleMedium"
+          className="mt-4 font-bold"
+          numberOfLines={1}
+          testID={getArrayTestId(
+            PrayerRequestCardTestIds.requestTitle,
+            prayerRequest.prayerRequestId,
+          )}
+        >
+          {prayerRequest.requestTitle}
+        </Text>
+
+        <Text
+          variant="bodyMedium"
+          className="mt-2"
+          numberOfLines={3}
+          testID={getArrayTestId(
+            PrayerRequestCardTestIds.requestDescription,
+            prayerRequest.prayerRequestId,
+          )}
+        >
+          {prayerRequest.requestDescription}
+        </Text>
+
+        <View className="flex flex-row justify-between items-center mt-6">
+          <View className="flex flex-row gap-x-3 items-center">
+            <Button
+              mode="outlined"
+              icon={likeIcon}
+              loading={isLikeLoading}
+              onPress={onLikePress}
+              testID={getArrayTestId(
+                PrayerRequestCardTestIds.likeButton,
+                prayerRequest.prayerRequestId,
+              )}
+            >
+              {formatNumber(
+                prayerRequest.likeCount ?? 0,
+                i18n.language as CultureCode,
+                { notation: "compact", compactDisplay: "short" },
+              )}
+            </Button>
+
+            <Button
+              mode="outlined"
+              icon={prayerRequest.userCommentId ? "comment" : "comment-outline"}
+              testID={getArrayTestId(
+                PrayerRequestCardTestIds.commentButton,
+                prayerRequest.prayerRequestId,
+              )}
+            >
+              {formatNumber(
+                prayerRequest.commentCount ?? 0,
+                i18n.language as CultureCode,
+                { notation: "compact", compactDisplay: "short" },
+              )}
+            </Button>
           </View>
-        )}
 
-        {prayerRequest.createdDate && (
-          <Text
-            variant="bodyMedium"
-            className="ml-4 text-gray-500"
-            testID={getArrayTestId(
-              PrayerRequestCardTestIds.createdDate,
-              prayerRequest.prayerRequestId,
-            )}
-          >
-            {formatDate(
-              prayerRequest.createdDate,
-              i18n.language as CultureCode,
-            )}
-          </Text>
-        )}
-      </View>
-
-      <Text
-        variant="titleMedium"
-        className="mt-4 font-bold"
-        numberOfLines={1}
-        testID={getArrayTestId(
-          PrayerRequestCardTestIds.requestTitle,
-          prayerRequest.prayerRequestId,
-        )}
-      >
-        {prayerRequest.requestTitle}
-      </Text>
-
-      <Text
-        variant="bodyMedium"
-        className="mt-2"
-        numberOfLines={3}
-        testID={getArrayTestId(
-          PrayerRequestCardTestIds.requestDescription,
-          prayerRequest.prayerRequestId,
-        )}
-      >
-        {prayerRequest.requestDescription}
-      </Text>
-
-      <View className="flex flex-row justify-between items-center mt-6">
-        <View className="flex flex-row gap-x-3 items-center">
           <Button
             mode="outlined"
-            icon={likeIcon}
-            loading={isLikeLoading}
-            onPress={onLikePress}
+            icon={prayerRequest.userPrayerSessionId ? "cross" : "cross-outline"}
+            onPress={() => openPrayerRequestActions(prayerRequest)}
             testID={getArrayTestId(
-              PrayerRequestCardTestIds.likeButton,
+              PrayerRequestCardTestIds.prayedButton,
               prayerRequest.prayerRequestId,
             )}
           >
             {formatNumber(
-              prayerRequest.likeCount ?? 0,
-              i18n.language as CultureCode,
-              { notation: "compact", compactDisplay: "short" },
-            )}
-          </Button>
-
-          <Button
-            mode="outlined"
-            icon={prayerRequest.userCommentId ? "comment" : "comment-outline"}
-            testID={getArrayTestId(
-              PrayerRequestCardTestIds.commentButton,
-              prayerRequest.prayerRequestId,
-            )}
-          >
-            {formatNumber(
-              prayerRequest.commentCount ?? 0,
+              prayerRequest.prayedCount ?? 0,
               i18n.language as CultureCode,
               { notation: "compact", compactDisplay: "short" },
             )}
           </Button>
         </View>
-
-        <Button
-          mode="outlined"
-          icon={prayerRequest.userPrayerSessionId ? "cross" : "cross-outline"}
-          onPress={() => openPrayerRequestActions(prayerRequest)}
-          testID={getArrayTestId(
-            PrayerRequestCardTestIds.prayedButton,
-            prayerRequest.prayerRequestId,
-          )}
-        >
-          {formatNumber(
-            prayerRequest.prayedCount ?? 0,
-            i18n.language as CultureCode,
-            { notation: "compact", compactDisplay: "short" },
-          )}
-        </Button>
       </View>
-    </View>
+
+      <PrayerRequestSkeleton />
+    </>
   );
 };
---
+++ components/prayer-request/prayer-request-skeleton.tsx
@@ -0,0 +1,24 @@
+import { Skeleton } from "moti/skeleton";
+import * as React from "react";
+import { View } from "react-native";
+import { useTheme } from "react-native-paper";
+
+export const PrayerRequestSkeleton: React.FC = () => {
+  const theme = useTheme();
+
+  return (
+    <View
+      className="p-5 border-b"
+      style={{
+        backgroundColor: theme.colors.background,
+        borderColor: theme.colors.outline,
+      }}
+    >
+      <View className="flex flex-row items-center">
+        <View className="flex flex-row items-center">
+          <Skeleton width={24} height={24} radius={"round"} />
+        </View>
+      </View>
+    </View>
+  );
+};

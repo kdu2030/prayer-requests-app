9573ef86dd0e7c82e90f063ee8b03603466c2eda
kdu2030 <kdu2030@gmail.com>
2026-04-05 22:20:42 -0400

    Add Prayer Request Page Container

---
+++ app/(drawer)/prayergroup/[id]/prayerrequest/[id].tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Text } from "react-native-paper";
+
+const PrayerRequestContainer: React.FC = () => {
+  return (
+    <>
+      <Text>Prayer request</Text>
+    </>
+  );
+};
+
+export default PrayerRequestContainer;

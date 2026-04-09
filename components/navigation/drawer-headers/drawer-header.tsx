bdae29a33e700da10b0de9b2f2bbcdb65d3d33da
kdu2030 <kdu2030@gmail.com>
2026-04-09 19:48:37 -0400

    Add Drawer Header

--- components/navigation/drawer-headers/drawer-header.tsx
+++ components/navigation/drawer-headers/drawer-header.tsx
@@ -1,23 +1,10 @@
-import { Ionicons } from "@expo/vector-icons";
 import { DrawerHeaderProps } from "@react-navigation/drawer";
-import { router, useSegments } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+import { useSegments } from "expo-router";
 import * as React from "react";
-import { TouchableOpacity, View } from "react-native";
-import { Text } from "react-native-paper";
-import { useTheme } from "react-native-paper";
-import { SafeAreaView } from "react-native-safe-area-context";
 
-import { useApiDataContext } from "../../../hooks/use-api-data";
-import { useI18N } from "../../../hooks/use-i18n";
-import { ProfilePicture } from "../../layouts/profile-picture";
 import { ROUTES_WITHOUT_HEADER } from "../drawer-constants";
 
-export const DrawerHeader: React.FC<DrawerHeaderProps> = ({ navigation }) => {
-  const { translate } = useI18N();
-  const { userData } = useApiDataContext();
-  const theme = useTheme();
-
+export const DrawerHeader: React.FC<DrawerHeaderProps> = (props) => {
   const segments = useSegments();
   const path = `/${segments.join("/")}`;
 
@@ -25,45 +12,5 @@ export const DrawerHeader: React.FC<DrawerHeaderProps> = ({ navigation }) => {
     return <></>;
   }
 
-  return (
-    <>
-      <StatusBar backgroundColor={theme.colors.primary} />
-
-      <SafeAreaView edges={["top"]}>
-        <View
-          className="flex flex-row items-center justify-between w-full px-4 py-4 grow"
-          style={{
-            backgroundColor: theme.colors.primary,
-          }}
-        >
-          <View className="flex flex-row items-center">
-            <TouchableOpacity
-              onPress={() => navigation.openDrawer()}
-              className="mr-2"
-            >
-              <Ionicons name="menu" size={28} color="white" />
-            </TouchableOpacity>
-            <Text variant="titleLarge" className="font-bold text-white">
-              {translate("common.appName")}
-            </Text>
-          </View>
-
-          <View className="flex flex-row items-center">
-            <TouchableOpacity
-              onPress={() => router.push("/(drawer)/search")}
-              className="mr-4"
-            >
-              <Ionicons name="search" size={28} color="white" />
-            </TouchableOpacity>
-
-            <ProfilePicture
-              url={userData?.image?.fileUrl}
-              width={36}
-              height={36}
-            />
-          </View>
-        </View>
-      </SafeAreaView>
-    </>
-  );
+  return <DrawerHeader {...props} />;
 };

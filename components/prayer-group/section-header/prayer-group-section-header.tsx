425a1b7474b18e0f77f0f82dd22e6cf47a6023a1
kdu2030 <kdu2030@gmail.com>
2026-04-11 18:37:08 -0400

    Add Profile Picture to Section Header

--- components/prayer-group/section-header/prayer-group-section-header.tsx
+++ components/prayer-group/section-header/prayer-group-section-header.tsx
@@ -6,31 +6,38 @@ import { View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { Text, useTheme } from "react-native-paper";
 
+import { useApiDataContext } from "../../../hooks/use-api-data";
+import { ProfilePicture } from "../../layouts/profile-picture";
+
 type Props = {
   title: string;
 };
 
 export const PrayerGroupSectionHeader: React.FC<Props> = ({ title }) => {
   const theme = useTheme();
+  const { userData } = useApiDataContext();
 
   return (
     <>
       <StatusBar backgroundColor={theme.colors.primary} />
       <View
-        className="flex-row items-center w-full p-4"
+        className="flex flex-row justify-between w-full p-4 items-center"
         style={{ backgroundColor: theme.colors.primary }}
       >
-        <TouchableOpacity onPress={() => router.back()}>
-          <MaterialCommunityIcons name="close" size={28} color="white" />
-        </TouchableOpacity>
+        <View className="flex-row items-center">
+          <TouchableOpacity onPress={() => router.back()}>
+            <MaterialCommunityIcons name="close" size={28} color="white" />
+          </TouchableOpacity>
 
-        <Text
-          variant="titleLarge"
-          className="ml-2 text-white font-bold"
-          numberOfLines={1}
-        >
-          {title}
-        </Text>
+          <Text
+            variant="titleLarge"
+            className="ml-2 text-white font-bold"
+            numberOfLines={1}
+          >
+            {title}
+          </Text>
+        </View>
+        <ProfilePicture url={userData?.image?.fileUrl} width={36} height={36} />
       </View>
     </>
   );

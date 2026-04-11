44fc7e99a282227414b9853b4a269023c03861d5
kdu2030 <kdu2030@gmail.com>
2026-04-11 18:15:12 -0400

    Connect Prayer Request Page

--- components/prayer-request/prayer-request-page.tsx
+++ components/prayer-request/prayer-request-page.tsx
@@ -2,9 +2,9 @@ import * as React from "react";
 import { useTheme } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { usePrayerGroupContext } from "../prayer-group/prayer-group-context";
 import { PrayerGroupSectionHeader } from "../prayer-group/section-header/prayer-group-section-header";
 import { PrayerRequestEntryPoint } from "./prayer-request-types";
+import { usePrayerRequestPage } from "./use-prayer-request-page";
 
 type Props = {
   prayerRequestId: number;
@@ -13,14 +13,16 @@ type Props = {
 
 export const PrayerRequestPage: React.FC<Props> = ({ prayerRequestId }) => {
   const theme = useTheme();
-  const { prayerGroupDetails } = usePrayerGroupContext();
+  const { prayerRequest } = usePrayerRequestPage(prayerRequestId);
 
   return (
     <SafeAreaView
       className="flex flex-1"
       style={{ backgroundColor: theme.colors.background }}
     >
-      <PrayerGroupSectionHeader title={prayerGroupDetails?.groupName ?? ""} />
+      <PrayerGroupSectionHeader
+        title={prayerRequest?.prayerGroup?.groupName ?? ""}
+      />
     </SafeAreaView>
   );
 };

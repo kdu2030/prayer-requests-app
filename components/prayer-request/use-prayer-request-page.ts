f6121c1fb3ee8f19df4686301eea5f9ea9cb1a79
kdu2030 <kdu2030@gmail.com>
2026-04-11 18:56:42 -0400

    Start On Add Prayer Request Like

--- components/prayer-request/use-prayer-request-page.ts
+++ components/prayer-request/use-prayer-request-page.ts
@@ -1,8 +1,12 @@
 import * as React from "react";
 
 import { useGetPrayerRequest } from "../../api/get-prayer-request";
+import { usePostPrayerRequestLike } from "../../api/post-prayer-request-like";
+import { useApiDataContext } from "../../hooks/use-api-data";
+import { useI18N } from "../../hooks/use-i18n";
 import { LoadStatus } from "../../types/api-response-types";
 import { PrayerRequestModel } from "../../types/prayer-request-types";
+import { useToasterContext } from "../toasters/toaster-context";
 
 export const usePrayerRequestPage = (prayerRequestId: number) => {
   const [prayerRequest, setPrayerRequest] =
@@ -10,7 +14,14 @@ export const usePrayerRequestPage = (prayerRequestId: number) => {
   const [prayerRequestLoadStatus, setPrayerRequestLoadStatus] =
     React.useState<LoadStatus>(LoadStatus.NotStarted);
 
+  const [isLikeLoading, setIsLikeLoading] = React.useState<boolean>(false);
+
+  const { userData } = useApiDataContext();
+  const { translate } = useI18N();
+  const { openToaster } = useToasterContext();
+
   const getPrayerRequest = useGetPrayerRequest();
+  const postPrayerRequestLike = usePostPrayerRequestLike();
 
   const loadPrayerRequest = React.useCallback(async () => {
     setPrayerRequestLoadStatus(LoadStatus.Loading);
@@ -28,6 +39,30 @@ export const usePrayerRequestPage = (prayerRequestId: number) => {
   React.useEffect(() => {
     loadPrayerRequest();
   }, [loadPrayerRequest]);
+
+  const addPrayerRequestLike = async () => {
+    if (!prayerRequest?.prayerRequestId || !userData?.userId) {
+      return;
+    }
+
+    setIsLikeLoading(true);
+    const response = await postPrayerRequestLike(
+      prayerRequest.prayerRequestId,
+      {
+        userId: userData.userId,
+        submittedDate: new Date().toISOString(),
+      },
+    );
+    setIsLikeLoading(false);
+
+    if (response.isError) {
+      openToaster({
+        message: translate("prayerRequest.addLike.failure"),
+        variant: "error",
+      });
+      return;
+    }
+  };
 
   return {
     prayerRequest,

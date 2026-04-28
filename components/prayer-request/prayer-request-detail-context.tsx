9d6eaf985b2390e2e00b5ebb70589819549b4ebc
kdu2030 <kdu2030@gmail.com>
2026-04-27 21:33:37 -0400

    Add Implementation for Prayer Request Detail Context

--- components/prayer-request/prayer-request-detail-context.tsx
+++ components/prayer-request/prayer-request-detail-context.tsx
@@ -5,19 +5,76 @@ import { PrayerRequestModel } from "../../types/prayer-request-types";
 export type PrayerRequestDetailContextType = {
   prayerRequests: Record<number, PrayerRequestModel>;
   getPrayerRequest: (prayerRequestId: number) => PrayerRequestModel | undefined;
-  updatePrayerRequest: (
+  setPrayerRequest: (
     prayerRequestId: number,
     prayerRequest: PrayerRequestModel,
   ) => void;
   addPrayerRequests: (prayerRequests: PrayerRequestModel[]) => void;
-  clearPrayerRequest: (prayerRequestId: number) => void;
 };
 
 export const PrayerRequestDetailContext =
   React.createContext<PrayerRequestDetailContextType>({
     prayerRequests: {},
     getPrayerRequest: () => {},
-    updatePrayerRequest: () => {},
+    setPrayerRequest: () => {},
     addPrayerRequests: () => {},
-    clearPrayerRequest: () => {},
   });
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const PrayerRequestDetailContextProvider: React.FC<Props> = ({
+  children,
+}) => {
+  const [prayerRequests, setPrayerRequests] = React.useState<
+    Record<number, PrayerRequestModel>
+  >({});
+
+  const getPrayerRequest = (
+    prayerRequestId: number,
+  ): PrayerRequestModel | undefined => {
+    return prayerRequests[prayerRequestId];
+  };
+
+  const setPrayerRequest = (
+    prayerRequestId: number,
+    prayerRequest: PrayerRequestModel,
+  ) => {
+    setPrayerRequests((prayerRequests) => ({
+      ...prayerRequests,
+      [prayerRequestId]: prayerRequest,
+    }));
+  };
+
+  const addPrayerRequests = (prayerRequests: PrayerRequestModel[]) => {
+    setPrayerRequests((currentPrayerRequests) => {
+      const updatedPrayerRequests: Record<number, PrayerRequestModel> = {
+        ...currentPrayerRequests,
+      };
+
+      prayerRequests.forEach((prayerRequest) => {
+        if (!prayerRequest.prayerRequestId) {
+          return;
+        }
+
+        updatedPrayerRequests[prayerRequest.prayerRequestId] = prayerRequest;
+      });
+
+      return updatedPrayerRequests;
+    });
+  };
+
+  return (
+    <PrayerRequestDetailContext.Provider
+      value={{
+        addPrayerRequests,
+        prayerRequests,
+        setPrayerRequest,
+        getPrayerRequest,
+      }}
+    >
+      {children}
+    </PrayerRequestDetailContext.Provider>
+  );
+};

252cb702ff51f23e7d98c317de97b6c68e428772
kdu2030 <kdu2030@gmail.com>
2026-05-01 21:25:45 -0400

    Add Comment Section

--- components/prayer-request/prayer-request-page.tsx
+++ components/prayer-request/prayer-request-page.tsx
@@ -1,9 +1,12 @@
+import { Formik } from "formik";
 import * as React from "react";
+import { View } from "react-native";
 import { useTheme } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { useI18N } from "../../hooks/use-i18n";
 import { LoadStatus } from "../../types/api-response-types";
+import { TextInput } from "../inputs/text-input";
 import { ErrorScreen } from "../layouts/error-screen";
 import { SpinnerScreen } from "../layouts/spinner-screen";
 import { PrayerGroupSectionHeader } from "../prayer-group/section-header/prayer-group-section-header";
@@ -33,46 +36,55 @@ export const PrayerRequestPage: React.FC<Props> = ({ prayerRequestId }) => {
   } = usePrayerRequestPage(prayerRequestId);
 
   return (
-    <SafeAreaView
-      className="flex flex-1"
-      style={{ backgroundColor: theme.colors.background }}
-    >
-      <PrayerGroupSectionHeader
-        title={prayerRequest?.prayerGroup?.groupName ?? ""}
-      />
+    <Formik initialValues={{}} onSubmit={() => {}}>
+      <SafeAreaView
+        className="flex flex-1"
+        style={{ backgroundColor: theme.colors.background }}
+      >
+        <PrayerGroupSectionHeader title={""} />
 
-      {prayerRequestLoadStatus === LoadStatus.Loading && (
-        <SpinnerScreen
-          loadingLabel={translate("prayerRequestPage.loading")}
-          showSafeArea={false}
+        {prayerRequestLoadStatus === LoadStatus.Loading && (
+          <SpinnerScreen
+            loadingLabel={translate("prayerRequestPage.loading")}
+            showSafeArea={false}
+          />
+        )}
+
+        {prayerRequestLoadStatus === LoadStatus.Error && (
+          <ErrorScreen
+            errorLabel={translate("prayerRequestPage.failedToLoad")}
+            onRetry={loadPrayerRequest}
+            showSafeArea={false}
+          />
+        )}
+
+        {prayerRequestLoadStatus === LoadStatus.Success && prayerRequest && (
+          <PrayerRequestCard
+            prayerRequest={prayerRequest}
+            onOpenMenu={openPrayerRequestMenu}
+            isLikeLoading={isLikeLoading}
+            onLikePress={onLikePress}
+            onPrayPress={openBookmarkBottomSheet}
+            showCreatedUser
+          />
+        )}
+
+        <View
+          className="flex flex-row w-full p-4 mt-auto border-t"
+          style={{ borderColor: theme.colors.outline }}
+        >
+          <View className="w-4/5 mt-auto">
+            <TextInput name="comment" label="comment" multiline />
+          </View>
+        </View>
+
+        <PrayerRequestActions
+          isOpen={isPrayerRequestActionsOpen}
+          showExtendedActions={showExtendedActions}
+          selectedPrayerRequest={prayerRequest}
+          onClose={closePrayerRequestActions}
         />
-      )}
-
-      {prayerRequestLoadStatus === LoadStatus.Error && (
-        <ErrorScreen
-          errorLabel={translate("prayerRequestPage.failedToLoad")}
-          onRetry={loadPrayerRequest}
-          showSafeArea={false}
-        />
-      )}
-
-      {prayerRequestLoadStatus === LoadStatus.Success && prayerRequest && (
-        <PrayerRequestCard
-          prayerRequest={prayerRequest}
-          onOpenMenu={openPrayerRequestMenu}
-          isLikeLoading={isLikeLoading}
-          onLikePress={onLikePress}
-          onPrayPress={openBookmarkBottomSheet}
-          showCreatedUser
-        />
-      )}
-
-      <PrayerRequestActions
-        isOpen={isPrayerRequestActionsOpen}
-        showExtendedActions={showExtendedActions}
-        selectedPrayerRequest={prayerRequest}
-        onClose={closePrayerRequestActions}
-      />
-    </SafeAreaView>
+      </SafeAreaView>
+    </Formik>
   );
 };

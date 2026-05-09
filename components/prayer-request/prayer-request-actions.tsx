c9c875dee5f4f32816953685fdbef73ab14ff09e
kdu2030 <kdu2030@gmail.com>
2026-05-09 17:39:13 -0400

    Add AppBottomSheet to Prayer Request Actions

--- components/prayer-request/prayer-request-actions.tsx
+++ components/prayer-request/prayer-request-actions.tsx
@@ -1,16 +1,11 @@
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
-import BottomSheet, {
-  BottomSheetBackdrop,
-  BottomSheetBackdropProps,
-  BottomSheetView,
-} from "@gorhom/bottom-sheet";
-import { BottomSheetMethods } from "@gorhom/bottom-sheet/lib/typescript/types";
 import * as React from "react";
 import { View } from "react-native";
 import { useTheme } from "react-native-paper";
 
 import { useI18N } from "../../hooks/use-i18n";
 import { PrayerRequestModel } from "../../types/prayer-request-types";
+import { AppBottomSheet } from "../layouts/app-bottom-sheet";
 import { PrayerGroupOptionButton } from "../prayer-group/options/prayer-group-option-button";
 import { usePrayerRequestActions } from "./use-prayer-request-actions";
 
@@ -34,78 +29,42 @@ export const PrayerRequestActions: React.FC<Props> = ({
     selectedPrayerRequest,
   );
 
-  const bottomSheetRef = React.useRef<BottomSheetMethods>(null);
-
-  React.useEffect(() => {
-    if (!bottomSheetRef.current) {
-      return;
-    }
-
-    if (isOpen) {
-      bottomSheetRef.current.snapToIndex(0);
-    } else {
-      bottomSheetRef.current.close();
-    }
-  }, [isOpen]);
-
-  const renderBackdrop = React.useCallback(
-    (props: BottomSheetBackdropProps) => {
-      return (
-        <BottomSheetBackdrop
-          {...props}
-          disappearsOnIndex={-1}
-          appearsOnIndex={0}
-        />
-      );
-    },
-    [],
-  );
-
   return (
-    <BottomSheet
-      enableDynamicSizing
-      index={-1}
-      backdropComponent={renderBackdrop}
-      backgroundStyle={{ backgroundColor: theme.colors.background }}
-      onClose={onClose}
-      ref={bottomSheetRef}
-    >
-      <BottomSheetView>
-        <View className="p-4">
-          <PrayerGroupOptionButton
-            icon={
-              <MaterialCommunityIcons
-                name="cross"
-                size={24}
-                color={theme.colors.onSurface}
-              />
-            }
-            label={translate("prayerRequest.actions.prayForRequest")}
-            onPress={() => {}}
-          />
+    <AppBottomSheet isOpen={isOpen} onClose={onClose}>
+      <View className="p-4">
+        <PrayerGroupOptionButton
+          icon={
+            <MaterialCommunityIcons
+              name="cross"
+              size={24}
+              color={theme.colors.onSurface}
+            />
+          }
+          label={translate("prayerRequest.actions.prayForRequest")}
+          onPress={() => {}}
+        />
 
-          <PrayerGroupOptionButton
-            icon={
-              <MaterialIcons
-                name={
-                  selectedPrayerRequest?.userBookmarkId
-                    ? "bookmark"
-                    : "bookmark-outline"
-                }
-                size={24}
-                color={theme.colors.onSurface}
-              />
-            }
-            label={
-              selectedPrayerRequest?.userBookmarkId
-                ? translate("prayerRequest.actions.unsavePrayerRequest")
-                : translate("prayerRequest.actions.savePrayerRequest")
-            }
-            onPress={toggleBookmark}
-            isLoading={isToggleBookmarkLoading}
-          />
-        </View>
-      </BottomSheetView>
-    </BottomSheet>
+        <PrayerGroupOptionButton
+          icon={
+            <MaterialIcons
+              name={
+                selectedPrayerRequest?.userBookmarkId
+                  ? "bookmark"
+                  : "bookmark-outline"
+              }
+              size={24}
+              color={theme.colors.onSurface}
+            />
+          }
+          label={
+            selectedPrayerRequest?.userBookmarkId
+              ? translate("prayerRequest.actions.unsavePrayerRequest")
+              : translate("prayerRequest.actions.savePrayerRequest")
+          }
+          onPress={toggleBookmark}
+          isLoading={isToggleBookmarkLoading}
+        />
+      </View>
+    </AppBottomSheet>
   );
 };

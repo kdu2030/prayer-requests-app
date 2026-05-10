d5519bdd9f0638f7ac8efe5168cb576f7b6778fd
kdu2030 <kdu2030@gmail.com>
2026-05-10 17:08:12 -0400

    Add Remaining Mocks

--- components/prayer-request-page/tests/prayer-request-page.test.tsx
+++ components/prayer-request-page/tests/prayer-request-page.test.tsx
@@ -2,6 +2,17 @@ import { englishTranslations } from "../../../i18n/en-us";
 import { mockUserData } from "../../prayer-group/tests/mock-data";
 import { PrayerRequestPage } from "../prayer-request-page";
 
+const mockSetPrayerRequestGlobal = jest.fn();
+const mockGetPrayerRequestFromStore = jest.fn();
+
+const mockGetPrayerRequest = jest.fn();
+const mockPostPrayerRequestLike = jest.fn();
+const mockDeletePrayerRequestLike = jest.fn();
+
+const mockPostPrayerRequestComment = jest.fn();
+const mockPutPrayerRequestComment = jest.fn();
+const mockDeletePrayerRequestComment = jest.fn();
+
 jest.mock("../../../hooks/use-api-data", () => ({
   useApiDataContext: () => {
     return {
@@ -21,5 +32,36 @@ jest.mock("../../../hooks/use-i18n", () => ({
 }));
 
 jest.mock("../../toasters/toaster-context");
+
+jest.mock("../../prayer-request/prayer-request-detail-context", () => ({
+  usePrayerRequestDetailContext: () => ({
+    setPrayerRequest: mockSetPrayerRequestGlobal,
+    getPrayerRequestFromStore: mockGetPrayerRequestFromStore,
+  }),
+}));
+
+jest.mock("../../../api/get-prayer-request", () => ({
+  useGetPrayerRequest: () => mockGetPrayerRequest,
+}));
+
+jest.mock("../../../api/post-prayer-request-like", () => ({
+  usePostPrayerRequestLike: () => mockPostPrayerRequestLike,
+}));
+
+jest.mock("../../../api/delete-prayer-request-like", () => ({
+  useDeletePrayerRequestLike: () => mockDeletePrayerRequestLike,
+}));
+
+jest.mock("../../../api/post-prayer-request-comment", () => ({
+  usePostPrayerRequestComment: () => mockPostPrayerRequestComment,
+}));
+
+jest.mock("../../../api/put-prayer-request-comment", () => ({
+  usePutPrayerRequestComment: () => mockPutPrayerRequestComment,
+}));
+
+jest.mock("../../../api/delete-prayer-request-comment", () => ({
+  useDeletePrayerRequestComment: () => mockDeletePrayerRequestComment,
+}));
 
 describe(PrayerRequestPage, () => {});

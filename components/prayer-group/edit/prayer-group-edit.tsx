785a6d190146c2f8cd4d4817731013315da14ff9
kdu2030 <kdu2030@gmail.com>
2026-06-17 21:33:57 -0400

    Add Prayer Group Section Header

--- components/prayer-group/edit/prayer-group-edit.tsx
+++ components/prayer-group/edit/prayer-group-edit.tsx
@@ -40,163 +40,170 @@ export const PrayerGroupEdit: React.FC = () => {
   }
 
   return (
-    <SafeAreaView
-      className="flex flex-1"
-      style={{ backgroundColor: theme.colors.background }}
-    >
+    <>
       <PrayerGroupSectionHeader
         title={translate("prayerGroup.edit.header", {
           groupName: prayerGroupDetails?.groupName,
         })}
       />
-
-      <ScrollView automaticallyAdjustKeyboardInsets>
-        <View className="p-4">
-          <Formik
-            initialValues={prayerGroupDetails ?? {}}
-            validationSchema={prayerGroupDetailsValidationSchema(
-              translate,
-              i18n.language as CultureCode,
-            )}
-            onSubmit={savePrayerGroupEdit}
-            validateOnBlur
-            validateOnChange={false}
-            innerRef={formikRef}
-          >
-            {({ values, errors, submitForm }) => (
-              <>
-                <Button
-                  mode="contained"
-                  className="self-end"
-                  loading={isLoading}
-                  onPress={submitForm}
-                  testID={PrayerGroupEditTestIds.saveButton}
-                >
-                  {translate("common.actions.save")}
-                </Button>
-
-                <Text className="font-bold mb-3" variant="bodyLarge">
-                  {translate("common.actions.preview")}
-                </Text>
-
-                <GroupPreview
-                  profilePictureUri={values?.avatarFile?.fileUrl}
-                  bannerUri={values?.bannerFile?.fileUrl}
-                  groupName={values?.groupName ?? ""}
-                  description={values?.description ?? ""}
-                  bannerTestID={PrayerGroupEditTestIds.groupPreviewBanner}
-                  groupNameTestID={PrayerGroupEditTestIds.groupPreviewName}
-                  descriptionTestID={
-                    PrayerGroupEditTestIds.groupPreviewDescription
-                  }
-                />
-
-                <Text className="font-bold mt-5" variant="bodyLarge">
-                  {translate("prayerGroup.options.about")}
-                </Text>
-
-                <TextInput
-                  name="groupName"
-                  className="mt-3"
-                  label={translate(
-                    "createPrayerGroup.groupNameDescription.groupName",
-                  )}
-                  maxLength={TEXT_INPUT_MAX_LENGTH}
-                  required
-                  testID={PrayerGroupEditTestIds.groupNameInput}
-                />
-
-                <TextInput
-                  name="description"
-                  className="mt-5"
-                  label={translate(
-                    "createPrayerGroup.groupNameDescription.description",
-                  )}
-                  multiline
-                  numberOfLines={5}
-                  required
-                  testID={PrayerGroupEditTestIds.descriptionInput}
-                />
-
-                <TextInput
-                  name="rules"
-                  className="mt-5"
-                  label={translate("createPrayerGroup.rules.label")}
-                  multiline
-                  numberOfLines={5}
-                />
-
-                <FormikSelect
-                  mode="flat"
-                  containerClassName="mt-5"
-                  name="visibilityLevel"
-                  options={visibilityOptions}
-                  label={translate("createPrayerGroup.visibilityLevel.label")}
-                  required
-                />
-
-                <Text variant="bodyLarge" className="font-bold mt-5">
-                  {translate("prayerGroup.edit.images")}
-                </Text>
-
-                <View className="mt-5 flex flex-row items-center">
-                  <Text variant="bodyLarge" className="w-1/2">
-                    {translate("createPrayerGroup.groupImageColorStep.image")}
-                  </Text>
+      <SafeAreaView
+        className="flex flex-1"
+        style={{ backgroundColor: theme.colors.background }}
+      >
+        <ScrollView automaticallyAdjustKeyboardInsets>
+          <View className="p-4">
+            <Formik
+              initialValues={prayerGroupDetails ?? {}}
+              validationSchema={prayerGroupDetailsValidationSchema(
+                translate,
+                i18n.language as CultureCode,
+              )}
+              onSubmit={savePrayerGroupEdit}
+              validateOnBlur
+              validateOnChange={false}
+              innerRef={formikRef}
+            >
+              {({ values, errors, submitForm }) => (
+                <>
                   <Button
-                    mode="outlined"
-                    className="w-1/2"
-                    onPress={() => selectImage("avatarFile", [1, 1])}
+                    mode="contained"
+                    className="self-end"
+                    loading={isLoading}
+                    onPress={submitForm}
+                    testID={PrayerGroupEditTestIds.saveButton}
                   >
-                    {translate(
-                      "createPrayerGroup.groupImageColorStep.selectImage",
-                    )}
+                    {translate("common.actions.save")}
                   </Button>
-                </View>
 
-                {values.avatarFile && (
-                  <>
-                    <SelectedImageCard
-                      onRemoveImage={() => clearField("avatarFile")}
-                      fileName={values.avatarFile.fileName ?? ""}
-                    />
-                    {errors.avatarFile && (
-                      <HelperText type="error">{errors.avatarFile}</HelperText>
-                    )}
-                  </>
-                )}
-
-                <View className="mt-6 flex flex-row items-center">
-                  <Text variant="bodyLarge" className="w-1/2">
-                    {translate("createPrayerGroup.groupImageColorStep.banner")}
+                  <Text className="font-bold mb-3" variant="bodyLarge">
+                    {translate("common.actions.preview")}
                   </Text>
-                  <Button
-                    mode="outlined"
-                    className="w-1/2"
-                    onPress={() => selectImage("bannerFile", [10, 3])}
-                  >
-                    {translate(
-                      "createPrayerGroup.groupImageColorStep.selectImage",
-                    )}
-                  </Button>
-                </View>
 
-                {values.bannerFile && (
-                  <>
-                    <SelectedImageCard
-                      onRemoveImage={() => clearField("bannerFile")}
-                      fileName={values.bannerFile.fileName ?? ""}
-                    />
-                    {errors.bannerFile && (
-                      <HelperText type="error">{errors.bannerFile}</HelperText>
+                  <GroupPreview
+                    profilePictureUri={values?.avatarFile?.fileUrl}
+                    bannerUri={values?.bannerFile?.fileUrl}
+                    groupName={values?.groupName ?? ""}
+                    description={values?.description ?? ""}
+                    bannerTestID={PrayerGroupEditTestIds.groupPreviewBanner}
+                    groupNameTestID={PrayerGroupEditTestIds.groupPreviewName}
+                    descriptionTestID={
+                      PrayerGroupEditTestIds.groupPreviewDescription
+                    }
+                  />
+
+                  <Text className="font-bold mt-5" variant="bodyLarge">
+                    {translate("prayerGroup.options.about")}
+                  </Text>
+
+                  <TextInput
+                    name="groupName"
+                    className="mt-3"
+                    label={translate(
+                      "createPrayerGroup.groupNameDescription.groupName",
                     )}
-                  </>
-                )}
-              </>
-            )}
-          </Formik>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+                    maxLength={TEXT_INPUT_MAX_LENGTH}
+                    required
+                    testID={PrayerGroupEditTestIds.groupNameInput}
+                  />
+
+                  <TextInput
+                    name="description"
+                    className="mt-5"
+                    label={translate(
+                      "createPrayerGroup.groupNameDescription.description",
+                    )}
+                    multiline
+                    numberOfLines={5}
+                    required
+                    testID={PrayerGroupEditTestIds.descriptionInput}
+                  />
+
+                  <TextInput
+                    name="rules"
+                    className="mt-5"
+                    label={translate("createPrayerGroup.rules.label")}
+                    multiline
+                    numberOfLines={5}
+                  />
+
+                  <FormikSelect
+                    mode="flat"
+                    containerClassName="mt-5"
+                    name="visibilityLevel"
+                    options={visibilityOptions}
+                    label={translate("createPrayerGroup.visibilityLevel.label")}
+                    required
+                  />
+
+                  <Text variant="bodyLarge" className="font-bold mt-5">
+                    {translate("prayerGroup.edit.images")}
+                  </Text>
+
+                  <View className="mt-5 flex flex-row items-center">
+                    <Text variant="bodyLarge" className="w-1/2">
+                      {translate("createPrayerGroup.groupImageColorStep.image")}
+                    </Text>
+                    <Button
+                      mode="outlined"
+                      className="w-1/2"
+                      onPress={() => selectImage("avatarFile", [1, 1])}
+                    >
+                      {translate(
+                        "createPrayerGroup.groupImageColorStep.selectImage",
+                      )}
+                    </Button>
+                  </View>
+
+                  {values.avatarFile && (
+                    <>
+                      <SelectedImageCard
+                        onRemoveImage={() => clearField("avatarFile")}
+                        fileName={values.avatarFile.fileName ?? ""}
+                      />
+                      {errors.avatarFile && (
+                        <HelperText type="error">
+                          {errors.avatarFile}
+                        </HelperText>
+                      )}
+                    </>
+                  )}
+
+                  <View className="mt-6 flex flex-row items-center">
+                    <Text variant="bodyLarge" className="w-1/2">
+                      {translate(
+                        "createPrayerGroup.groupImageColorStep.banner",
+                      )}
+                    </Text>
+                    <Button
+                      mode="outlined"
+                      className="w-1/2"
+                      onPress={() => selectImage("bannerFile", [10, 3])}
+                    >
+                      {translate(
+                        "createPrayerGroup.groupImageColorStep.selectImage",
+                      )}
+                    </Button>
+                  </View>
+
+                  {values.bannerFile && (
+                    <>
+                      <SelectedImageCard
+                        onRemoveImage={() => clearField("bannerFile")}
+                        fileName={values.bannerFile.fileName ?? ""}
+                      />
+                      {errors.bannerFile && (
+                        <HelperText type="error">
+                          {errors.bannerFile}
+                        </HelperText>
+                      )}
+                    </>
+                  )}
+                </>
+              )}
+            </Formik>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 };

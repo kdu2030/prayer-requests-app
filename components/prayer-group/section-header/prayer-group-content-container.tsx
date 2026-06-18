516aa0c6dcd3dd0252d1420305342da5e748bade
kdu2030 <kdu2030@gmail.com>
2026-06-17 21:39:31 -0400

    Add Prayer Group Content Container

--- components/prayer-group/section-header/prayer-group-content-container.tsx
+++ components/prayer-group/section-header/prayer-group-content-container.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useTheme } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { PrayerGroupSectionHeader } from "./prayer-group-section-header";
@@ -12,10 +13,16 @@ export const PrayerGroupContentContainer: React.FC<Props> = ({
   title,
   children,
 }) => {
+  const theme = useTheme();
+
   return (
     <>
       <PrayerGroupSectionHeader title={title} />
-      <SafeAreaView className="flex flex-1" edges={["bottom", "left", "right"]}>
+      <SafeAreaView
+        className="flex flex-1"
+        edges={["bottom", "left", "right"]}
+        style={{ backgroundColor: theme.colors.background }}
+      >
         {children}
       </SafeAreaView>
     </>

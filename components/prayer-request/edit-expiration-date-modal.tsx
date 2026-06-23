76449975f5f319ba80dd6d351e0b4318fe91f1d9
kdu2030 <kdu2030@gmail.com>
2026-06-22 22:17:33 -0400

    Add Cancel and Save Buttons

--- components/prayer-request/edit-expiration-date-modal.tsx
+++ components/prayer-request/edit-expiration-date-modal.tsx
@@ -1,9 +1,11 @@
 import { Formik } from "formik";
 import * as React from "react";
+import { View } from "react-native";
 import { Text } from "react-native-paper";
 
 import { useI18N } from "../../hooks/use-i18n";
 import { PrayerRequestModel } from "../../types/prayer-request-types";
+import { DismissButton } from "../inputs/dismiss-button";
 import { FormikSelect } from "../inputs/formik-select";
 import { RoundedModal } from "../modals/rounded-modal";
 import { EditExpirationDateForm } from "./prayer-request-types";
@@ -47,6 +49,16 @@ export function EditExpirationDateModal({
               options={expirationDateOptions}
               label={translate("prayerRequest.editExpirationDate.extendBy")}
             />
+
+            <View className="flex flex-row gap-2 mt-6 self-end">
+              <DismissButton mode="outlined" onPress={onClose}>
+                {translate("common.actions.cancel")}
+              </DismissButton>
+
+              <DismissButton mode="contained" onPress={() => {}}>
+                {translate("common.actions.save")}
+              </DismissButton>
+            </View>
           </>
         )}
       </Formik>

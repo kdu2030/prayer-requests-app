f7c3b79350c5b2772f40076b5f79a33ed6d0a31e
kdu2030 <kdu2030@gmail.com>
2026-07-26 22:13:28 -0400

    Add Loaded Prayer Requests

--- components/home/use-user-home-page.ts
+++ components/home/use-user-home-page.ts
@@ -1,12 +1,14 @@
 import * as React from "react";
 
 import { useApiDataContext } from "../../hooks/use-api-data";
+import { PrayerRequestModel } from "../../types/prayer-request-types";
 import { getJoinedPrayerGroups } from "../prayer-group/prayer-group-helpers";
 import { useGlobalPrayerRequestsContext } from "../prayer-request/global-prayer-requests-context";
 
 export const useUserHomePage = () => {
   const { userData } = useApiDataContext();
   const [prayerRequestIds, setPrayerRequestIds] = React.useState<number[]>([]);
+  const { getPrayerRequestFromStore } = useGlobalPrayerRequestsContext();
 
   const joinedPrayerGroups = React.useMemo(() => {
     return getJoinedPrayerGroups(userData?.prayerGroups ?? []);
@@ -14,5 +16,25 @@ export const useUserHomePage = () => {
 
   const joinedNoPrayerGroups = joinedPrayerGroups.length === 0;
 
-  return { joinedPrayerGroups, joinedNoPrayerGroups };
+  const loadedPrayerRequests = React.useMemo(() => {
+    return prayerRequestIds.reduce(
+      (prayerRequests: PrayerRequestModel[], prayerRequestId) => {
+        const prayerRequest = getPrayerRequestFromStore(prayerRequestId);
+        if (!prayerRequest) {
+          return prayerRequests;
+        }
+
+        prayerRequests.push(prayerRequest);
+        return prayerRequests;
+      },
+      [],
+    );
+  }, [getPrayerRequestFromStore, prayerRequestIds]);
+
+  return {
+    joinedPrayerGroups,
+    joinedNoPrayerGroups,
+    loadedPrayerRequests,
+    setPrayerRequestIds,
+  };
 };
